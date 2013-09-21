1905b111d3b615daa79c0b213e3d87ae6a5ec055
jrmartin <jesus@metacell.us>
2013-09-21 11:21:50 -0700

    Building tests , moving inside parent folder.

--- src/main/webapp/js/tests/GeppettoTests.js
+++ src/main/webapp/js/tests/GeppettoTests.js
@@ -1,10 +1,14 @@
-module( "Commands Test", {
+module( "Geppetto Client Testing", {
     setup: function() {
     }, teardown: function() {
     }
 });
 
-test( "Global Object Test", function() {
+test("Global scope Test", function(){
+	notEqual(help(), null, "Global help() command available, passed");
+});
+
+test( "G Object Test", function() {
 	GEPPETTO.Main.init();
 	equal(G.getCurrentSimulation(), NO_SIMULATION_TO_GET, "No simulation, passed.");
 	    
@@ -16,9 +20,19 @@ test( "Global Object Test", function() {
 	
 	notEqual(G.help(), null, "Help command for object G is available, passed.");
 	
-	notEqual(help(), null, "Global help() command available, passed");
+	var autocompleteWasCalled = false;
+	var oldAutocomplete = $.fn.extend(true, {}, $.autocomplete);
+	$.autocomplete = function(){
+	    autocompleteWasCalled = true;
+	};
 	
-	//GEPPETTO.Console.createConsole();
+	$.fn.autocomplete = oldAutocomplete;
+
+	ok(autocompleteWasCalled,"Should call autocomplete.");
+
+	
+	GEPPETTO.Console.createConsole();
+	
 	equal(G.clear(),CLEAR_HISTORY, "Console cleared");
 	
 	equal(G.copyToClipboard(), EMPTY_CONSOLE_HISTORY, "No commands to copy, test passed");
@@ -26,13 +40,19 @@ test( "Global Object Test", function() {
 });
 
 test( "Simulation Test", function() {
+	notEqual(Simulation.help(), null, "Help command for Simulation object is available, passed.");
+	
 	equal(getSimulationStatus(), Simulation.StatusEnum.INIT, "Simulation in initial conditions, passed.");
+	
 	Simulation.load("https://dl.dropboxusercontent.com/s/2oczzgnheple0mk/sph-sim-config.xml?token_hash=AAGbu0cCNW8zK_2DUoc0BPuCpspGqcNRIfk-6uDCMVUiHA");
 	equal( getSimulationStatus(),Simulation.StatusEnum.LOADED, "Simulation Loaded, passed");
+	
 	Simulation.start();
 	equal( getSimulationStatus(),Simulation.StatusEnum.STARTED, "Simulation Started, passed");
+	
 	Simulation.pause();
 	equal( getSimulationStatus(),Simulation.StatusEnum.PAUSED, "Simulation Paused, passed");
+	
 	Simulation.stop();
 	equal( getSimulationStatus(),Simulation.StatusEnum.STOPPED, "Simulation Stopped, passed");	
 });

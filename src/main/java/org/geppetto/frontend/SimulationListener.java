3a0ac5c1489094fbc983074a12aed6714fb19e9b
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-11-29 16:31:43 +0000

    Updated to user refactored SUBTREE

--- src/main/java/org/geppetto/frontend/SimulationListener.java
+++ src/main/java/org/geppetto/frontend/SimulationListener.java
@@ -65,7 +65,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 /**
  * Class that handles the Web Socket connections the servlet is receiving.

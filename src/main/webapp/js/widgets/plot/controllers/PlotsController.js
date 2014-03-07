168bc149c202c90e026a9547ad193ee81eb9c838
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-03-07 14:49:26 +0000

    First implementation of plotting two variables one against the other

--- src/main/webapp/js/widgets/plot/controllers/PlotsController.js
+++ src/main/webapp/js/widgets/plot/controllers/PlotsController.js
@@ -155,8 +155,21 @@ PlotsController = {
 					var newValues = [];
 					
 					//create new values dataset
-					for(var x =0; x <dataSets.length; x++){
-						newValues.push({label : dataSets[x].label, data: [[simulationStates[dataSets[x].label].value]]});
+					for(var x =0; x <dataSets.length; x++)
+					{
+						var ds=dataSets[x].label.split("/");
+						if(ds.length==1)
+						{
+							newValues.push({label : dataSets[x].label, data: [[simulationStates[ds[0]].value]]});
+						}
+						if(ds.length==2)
+						{
+							newValues.push({label : dataSets[x].label, data: [[
+							                                                   simulationStates[ds[0]].value,
+							                                                   simulationStates[ds[1]].value
+							                                                   ]]});
+						}
+						
 					}
 					
 					//update plot with new data set

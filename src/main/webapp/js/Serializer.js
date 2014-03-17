1ec048aab7d483b74c1423f7a2eafc778d03258b
molson <molson@intrepidls.com>
2014-03-16 18:02:32 -0700

    Simplify Serializer#stringToObject

--- src/main/webapp/js/Serializer.js
+++ src/main/webapp/js/Serializer.js
@@ -37,112 +37,43 @@
  */
 define(function(require) {
 	return function(GEPPETTO) {
-
-		function arrayNode(parent, node, index, statePath) {
-			var iNumber = index[0].replace(/[\[\]']+/g, "");
-
-			//create array object
-			window[node] = [];
-
-			var c = window[node][parseInt(iNumber)] = {};
-
-			if(parent == null) {
-				var stateName = node + "[" + parseInt(iNumber) + "]";
-
-				for(var x = 1; x < statePath.length; x++) {
-					var child = statePath[x];
-					stateName = stateName + "." + child;
-
-					c = c[child] = new GEPPETTO.SimState.State(stateName);
-				}
-
-				c = new GEPPETTO.SimState.State(stateName, 0);
-
-				GEPPETTO.Simulation.simulationStates[stateName] = c;
-			}
-			else {
-				var stateName = node + "[" + parseInt(iNumber) + "]";
-				window[parent][node][parseInt(iNumber)] = new GEPPETTO.SimState.State(stateName);
-			}
-		}
-
-		function updateArrayNode(parent, node, index, statePath) {
-			var iNumber = index[0].replace(/[\[\]']+/g, "");
-
-			var c = window[node][parseInt(iNumber)];
-
-			var stateNamePath = node + "[" + parseInt(iNumber) + "]";
-
-			for(var x = 1; x < statePath.length; x++) {
-				var child = statePath[x];
-				stateNamePath = stateNamePath + "." + child;
-
-				if(c[child] == null) {
-					c[child] = new GEPPETTO.SimState.State(stateNamePath, 0);
-				}
-
-				c = c[child];
-			}
-
-			if(parent != null) {
-				stateNamePath = parent + "." + stateNamePath;
-			}
-
-			c = new GEPPETTO.SimState.State(stateNamePath, 0);
-
-			GEPPETTO.Simulation.simulationStates[stateNamePath] = c;
-		}
-
 		/**
 		 * Method responsible for serializing values to simulation states
 		 */
-
 		GEPPETTO.Serializer = {
-			stringToObject: function(parent, statePath) {
-				//get first node from path
-				var node = statePath[0];
 
-				//get index from node if it's array
-				var index = node.match(/[^[\]]+(?=])/g);
+			/**
+			 * Converts string representation of variable into object,
+			 * inserts them into the window object
+			 * Examples:
+			 *   "hhcell.electrical.hhpop[0].v"
+			 *   "hhcell.electrical.hhpop.v"
+			 *
+			 **/
+			stringToObject: function(input) {
+				var arrPattern = /(\w+)\[(\d*)\]$/;
 
-				//take index and brackets out of the equation for now
-				node = node.replace(/ *\[[^]]*\] */g, "");
-
-				if(window[node] == null) {
-					//we have an array
-					if(index != null) {
-						arrayNode(parent, node, index, statePath);
+				function index(obj, element) {
+					var matches = element.match(arrPattern);
+					if(!matches) {
+						if(!obj[element]) {
+							obj[element] = new GEPPETTO.SimState.State(element);
+						}
+						return obj[element];
 					}
 					else {
-						window[node] = new GEPPETTO.SimState.State(node, 0);
-
-						if(parent != null) {
-							window[parent][node] = new GEPPETTO.SimState.State(node, 0);
+						if(!obj[matches[1]]) {
+							obj[matches[1]] = [];
 						}
-
-						statePath.splice(0, 1);
-
-						this.stringToObject(node, statePath);
+						var arrIndex = parseInt(matches[2]);
+						if(!obj[matches[1]][arrIndex]) {
+							obj[matches[1]][arrIndex] = new GEPPETTO.SimState.State(element);
+						}
+						return obj[matches[1]][arrIndex];
 					}
 				}
-				else {
-					if(index != null) {
-						updateArrayNode(parent, node, index, statePath);
-					}
-					else {
-						window[node] = new GEPPETTO.SimState.State(node, 0);
-
-						if(parent != null) {
-							window[parent].push(window[node]);
-						}
-
-						statePath.splice(0, 1);
-
-						this.stringToObject(node, statePath);
-					}
-				}
+				input.split('.').reduce(index, window);
 			}
-
 		};
 
 	};

bd5262f55a6d6477f030604d583f6ea664f5d4c9
adrianq <adrianq@users.noreply.github.com>
2014-06-04 09:30:05 +0100

    Update config.js
    Delete old configuration code

--- src/main/webapp/js/widgets/scatter3d/config.js
+++ src/main/webapp/js/widgets/scatter3d/config.js
@@ -31,27 +31,3 @@ define(function(require) {
 		require("widgets/scatter3d/controllers/Scatter3dController")(GEPPETTO);
 	};
 });
-
-
-//define(function(require) {
-//	return function(GEPPETTO) {
-//		/**
-//		 * Load CSS File
-//		 * @param url
-//		 */
-//		function loadCss(url) {
-//			var link = document.createElement("link");
-//			link.type = "text/css";
-//			link.rel = "stylesheet";
-//			link.href = url;
-//			document.getElementsByTagName("head")[0].appendChild(link);
-//		}
-//		
-//		require("widgets/scatter3d/vendor/three.min");
-//		require("widgets/scatter3d/vendor/OrbitControls");
-//		require('widgets/scatter3d/controllers/Scatter3dController')(GEPPETTO);
-//
-//		loadCss("js/widgets/scatter3d/Scatter3d.css");
-//
-//	};
-//});

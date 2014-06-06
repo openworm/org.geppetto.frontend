ad001e403229dec8a7638dbd684284aeb29d0818
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-06-06 10:21:40 +0100

    version using dat for static data and watched variable and d3 for static data

--- src/main/webapp/js/widgets/treevisualiser/TreeVisualiser.js
+++ src/main/webapp/js/widgets/treevisualiser/TreeVisualiser.js
@@ -46,6 +46,8 @@ define(function(require) {
 		data: {},
 		variableToDisplay: null,
 		isDisplayed: false,
+		nodes: {},
+		links: [],
 		
 		defaultTreeVisualiserOptions:  {
 			mode: "d3", // Options: d3, dat
@@ -75,13 +77,19 @@ define(function(require) {
 	//			this.generateRealDataTestTreeForDAT();
 				
 	//			Testing With Variable
-	//			this.setData("hhcell");
+				this.setData("hhcell");
 				
 				this.dialog.append(this.gui.domElement);
 			}
 			else if (this.options.mode == 'd3'){
 	//			Testing Data
-				this.generateTestTreeForD3();
+//				this.generateTestTreeForD3();
+				
+	//			Testing With Real Data
+				this.generateRealDataTestTreeForD3();
+				
+				//			Testing With Variable
+				//this.setData("hhcell");
 			}
 			
 		},
@@ -98,24 +106,25 @@ define(function(require) {
 				}
 				else{
 					this.data = state;
+					var parent = '';
 					if (this.options.mode == 'dat'){
-						this.displayTree(this.gui, this.data);
+						parent = this.gui;
 					}	
-					else if (this.options.mode == 'd3') {
-						
-					}
+					
+					this.prepareTree(parent, this.data);
+					this.paintTree();
 				}
 			}
 
 			return "Metadata or variables to display added to tree visualiser";
 		},
 		
-		displayTree: function(parent, data){
+		prepareTree: function(parent, data){
 			if (this.options.mode == 'dat'){
 				for (var key in data){
 					if (data[key] !== null && typeof data[key] === 'object'){
 						parentFolder = parent.addFolder(key);
-						this.displayTree(parentFolder, data[key]);
+						this.prepareTree(parentFolder, data[key]);
 					}
 					else{
 						if (data[key] === null){data[key] = '';}
@@ -125,23 +134,31 @@ define(function(require) {
 			}
 			else if (this.options.mode == 'd3'){
 				for (var key in data){
+					nodeName = (parent != '')?parent + "." + key:key;
 					if (data[key] !== null && typeof data[key] === 'object'){
-						this.nodes[key] = {name: key};
-						
-						if (parent != null){
+						this.nodes[nodeName] = {name: key};
+						if (parent != ''){
 							var link = {};
-							link.source = parent;
-							link.target = this.nodes[key];
-							link.type = "licensing";
+							link.source = this.nodes[parent];
+							link.target = this.nodes[nodeName];
+							link.type = "suit";
 							this.links.push(link);
 						}
-						this.displayTree(this.nodes[key], data[key]);
+						this.prepareTree(nodeName, data[key]);
 					}
 					else{
 						if (data[key] === null){data[key] = '';}
-//						this.nodes[key] = {name: key + "=" + data[key]};
-						this.nodes[key] = {name: key};
+						this.nodes[nodeName] = {name: key + "=" + data[key]};
+						if (parent != ''){
+							var link = {};
+							link.source = this.nodes[parent];
+							link.target = this.nodes[nodeName];
+							link.type = "suit";
+							this.links.push(link);
+						}
+						
 					}
+					
 				}
 			}
 		},
@@ -151,75 +168,40 @@ define(function(require) {
 		updateData: function(){
 			if (this.variableToDisplay != null){
 				newdata = this.getState(GEPPETTO.Simulation.watchTree, this.variableToDisplay);
-				if (!this.isDisplayed){
-					this.data = newdata;
-					this.displayTree(this.gui, this.data);
-					this.isDisplayed = true;
+				if (this.options.mode == 'dat'){
+					if (!this.isDisplayed){
+						this.data = newdata;
+						this.prepareTree(this.gui, this.data);
+						this.isDisplayed = true;
+					}
+					else{
+						$.extend(true, this.data, newdata);
+					}
 				}
-				else{
-					$.extend(true, this.data, newdata);
+				else if (this.options.mode == 'd3'){
+					
 				}
 			}
 		},
-		generateTestTreeForD3: function(){
-				var links = [
-				  {source: "Microsoft", target: "Amazon", type: "licensing"},
-				  {source: "Microsoft", target: "HTC", type: "licensing"},
-				  {source: "Samsung", target: "Apple", type: "suit"},
-				  {source: "Motorola", target: "Apple", type: "suit"},
-				  {source: "Nokia", target: "Apple", type: "resolved"},
-				  {source: "HTC", target: "Apple", type: "suit"},
-				  {source: "Kodak", target: "Apple", type: "suit"},
-				  {source: "Microsoft", target: "Barnes & Noble", type: "suit"},
-				  {source: "Microsoft", target: "Foxconn", type: "suit"},
-				  {source: "Oracle", target: "Google", type: "suit"},
-				  {source: "Apple", target: "HTC", type: "suit"},
-				  {source: "Microsoft", target: "Inventec", type: "suit"},
-				  {source: "Samsung", target: "Kodak", type: "resolved"},
-				  {source: "LG", target: "Kodak", type: "resolved"},
-				  {source: "RIM", target: "Kodak", type: "suit"},
-				  {source: "Sony", target: "LG", type: "suit"},
-				  {source: "Kodak", target: "LG", type: "resolved"},
-				  {source: "Apple", target: "Nokia", type: "resolved"},
-				  {source: "Qualcomm", target: "Nokia", type: "resolved"},
-				  {source: "Apple", target: "Motorola", type: "suit"},
-				  {source: "Microsoft", target: "Motorola", type: "suit"},
-				  {source: "Motorola", target: "Microsoft", type: "suit"},
-				  {source: "Huawei", target: "ZTE", type: "suit"},
-				  {source: "Ericsson", target: "ZTE", type: "suit"},
-				  {source: "Kodak", target: "Samsung", type: "resolved"},
-				  {source: "Apple", target: "Samsung", type: "suit"},
-				  {source: "Kodak", target: "RIM", type: "suit"},
-				  {source: "Nokia", target: "Qualcomm", type: "suit"}
-				];
-
-				var nodes = {};
-
-				// Compute the distinct nodes from the links.
-				links.forEach(function(link) {
-				  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
-				  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
-				});
-				
-				
-				//this.displayTree(null, this.getTestingData());
-
+		
+		paintTree: function(){
+			if (this.options.mode == 'd3'){
 				var width = 960,
 				    height = 500;
-
+	
 				var force = d3.layout.force()
-				    .nodes(d3.values(nodes))
-				    .links(links)
+				    .nodes(d3.values(this.nodes))
+				    .links(this.links)
 				    .size([width, height])
 				    .linkDistance(60)
 				    .charge(-300)
 				    .on("tick", tick)
 				    .start();
-
+	
 				var svg = d3.select("#"+this.id).append("svg")
 				    .attr("width", width)
 				    .attr("height", height);
-
+	
 				// Per-type markers, as they don't inherit styles.
 				svg.append("defs").selectAll("marker")
 				    .data(["suit", "licensing", "resolved"])
@@ -233,59 +215,173 @@ define(function(require) {
 				    .attr("orient", "auto")
 				  .append("path")
 				    .attr("d", "M0,-5L10,0L0,5");
-
+	
 				var path = svg.append("g").selectAll("path")
 				    .data(force.links())
 				  .enter().append("path")
 				    .attr("class", function(d) { return "link " + d.type; })
 				    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
-
+	
 				var circle = svg.append("g").selectAll("circle")
 				    .data(force.nodes())
 				  .enter().append("circle")
 				    .attr("r", 6)
 				    .call(force.drag);
-
+	
 				var text = svg.append("g").selectAll("text")
 				    .data(force.nodes())
 				  .enter().append("text")
 				    .attr("x", 8)
 				    .attr("y", ".31em")
 				    .text(function(d) { return d.name; });
-
+	
 				// Use elliptical arc path segments to doubly-encode directionality.
 				function tick() {
 				  path.attr("d", linkArc);
 				  circle.attr("transform", transform);
 				  text.attr("transform", transform);
 				}
-
+	
 				function linkArc(d) {
 				  var dx = d.target.x - d.source.x,
 				      dy = d.target.y - d.source.y,
 				      dr = Math.sqrt(dx * dx + dy * dy);
 				  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
 				}
-
+	
 				function transform(d) {
 				  return "translate(" + d.x + "," + d.y + ")";
 				}
+			}	
 		},
 		
-		generateTestTreeForDAT: function(){
-			var FizzyText = function() {
-			  this.message = 'dat.gui';
-			  this.speed = 0.8;
-			  this.displayOutline = false;
-			};
-			
-			var text = new FizzyText();
-			this.gui.add(text, 'message');
-			this.gui.add(text, 'speed', -5, 5);
-			this.gui.add(text, 'displayOutline');	
-		},
+//		generateTestTreeForD3: function(){
+//				var links = [
+//				  {source: "Microsoft", target: "Amazon", type: "licensing"},
+//				  {source: "Microsoft", target: "HTC", type: "licensing"},
+//				  {source: "Samsung", target: "Apple", type: "suit"},
+//				  {source: "Motorola", target: "Apple", type: "suit"},
+//				  {source: "Nokia", target: "Apple", type: "resolved"},
+//				  {source: "HTC", target: "Apple", type: "suit"},
+//				  {source: "Kodak", target: "Apple", type: "suit"},
+//				  {source: "Microsoft", target: "Barnes & Noble", type: "suit"},
+//				  {source: "Microsoft", target: "Foxconn", type: "suit"},
+//				  {source: "Oracle", target: "Google", type: "suit"},
+//				  {source: "Apple", target: "HTC", type: "suit"},
+//				  {source: "Microsoft", target: "Inventec", type: "suit"},
+//				  {source: "Samsung", target: "Kodak", type: "resolved"},
+//				  {source: "LG", target: "Kodak", type: "resolved"},
+//				  {source: "RIM", target: "Kodak", type: "suit"},
+//				  {source: "Sony", target: "LG", type: "suit"},
+//				  {source: "Kodak", target: "LG", type: "resolved"},
+//				  {source: "Apple", target: "Nokia", type: "resolved"},
+//				  {source: "Qualcomm", target: "Nokia", type: "resolved"},
+//				  {source: "Apple", target: "Motorola", type: "suit"},
+//				  {source: "Microsoft", target: "Motorola", type: "suit"},
+//				  {source: "Motorola", target: "Microsoft", type: "suit"},
+//				  {source: "Huawei", target: "ZTE", type: "suit"},
+//				  {source: "Ericsson", target: "ZTE", type: "suit"},
+//				  {source: "Kodak", target: "Samsung", type: "resolved"},
+//				  {source: "Apple", target: "Samsung", type: "suit"},
+//				  {source: "Kodak", target: "RIM", type: "suit"},
+//				  {source: "Nokia", target: "Qualcomm", type: "suit"}
+//				];
+//
+//				var nodes = {};
+//
+//				// Compute the distinct nodes from the links.
+//				links.forEach(function(link) {
+//				  link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+//				  link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+//				});
+//				
+//				
+//				var width = 960,
+//				    height = 500;
+//
+//				var force = d3.layout.force()
+//				    .nodes(d3.values(nodes))
+//				    .links(links)
+//				    .size([width, height])
+//				    .linkDistance(60)
+//				    .charge(-300)
+//				    .on("tick", tick)
+//				    .start();
+//
+//				var svg = d3.select("#"+this.id).append("svg")
+//				    .attr("width", width)
+//				    .attr("height", height);
+//
+//				// Per-type markers, as they don't inherit styles.
+//				svg.append("defs").selectAll("marker")
+//				    .data(["suit", "licensing", "resolved"])
+//				  .enter().append("marker")
+//				    .attr("id", function(d) { return d; })
+//				    .attr("viewBox", "0 -5 10 10")
+//				    .attr("refX", 15)
+//				    .attr("refY", -1.5)
+//				    .attr("markerWidth", 6)
+//				    .attr("markerHeight", 6)
+//				    .attr("orient", "auto")
+//				  .append("path")
+//				    .attr("d", "M0,-5L10,0L0,5");
+//
+//				var path = svg.append("g").selectAll("path")
+//				    .data(force.links())
+//				  .enter().append("path")
+//				    .attr("class", function(d) { return "link " + d.type; })
+//				    .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+//
+//				var circle = svg.append("g").selectAll("circle")
+//				    .data(force.nodes())
+//				  .enter().append("circle")
+//				    .attr("r", 6)
+//				    .call(force.drag);
+//
+//				var text = svg.append("g").selectAll("text")
+//				    .data(force.nodes())
+//				  .enter().append("text")
+//				    .attr("x", 8)
+//				    .attr("y", ".31em")
+//				    .text(function(d) { return d.name; });
+//
+//				// Use elliptical arc path segments to doubly-encode directionality.
+//				function tick() {
+//				  path.attr("d", linkArc);
+//				  circle.attr("transform", transform);
+//				  text.attr("transform", transform);
+//				}
+//
+//				function linkArc(d) {
+//				  var dx = d.target.x - d.source.x,
+//				      dy = d.target.y - d.source.y,
+//				      dr = Math.sqrt(dx * dx + dy * dy);
+//				  return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+//				}
+//
+//				function transform(d) {
+//				  return "translate(" + d.x + "," + d.y + ")";
+//				}
+//		},
+//		
+//		generateTestTreeForDAT: function(){
+//			var FizzyText = function() {
+//			  this.message = 'dat.gui';
+//			  this.speed = 0.8;
+//			  this.displayOutline = false;
+//			};
+//			
+//			var text = new FizzyText();
+//			this.gui.add(text, 'message');
+//			this.gui.add(text, 'speed', -5, 5);
+//			this.gui.add(text, 'displayOutline');	
+//		},
 		
 		generateRealDataTestTreeForDAT: function(){
+			this.setData(this.getTestingData());
+		},
+		
+		generateRealDataTestTreeForD3: function(){
 			this.setData(this.getTestingData());
 		},
 		

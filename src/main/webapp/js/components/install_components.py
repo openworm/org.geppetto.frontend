3857caeccfd4e2df86ea10807bb07d1aca916ffb
Matt Olson <mattlolson@gmail.com>
2014-09-04 13:16:46 -0700

    Python script to install components, wire them up

---
+++ src/main/webapp/js/components/install_components.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import os, sys, json, distutils.core,subprocess, shutil, glob
+from subprocess import call
+
+serverHome = os.environ['SERVER_HOME']
+config = json.loads(open(os.path.join(os.path.dirname(__file__), 'bower.json')).read())
+componentsfile = open(os.path.join(os.path.dirname(__file__), 'components.js'),'w')
+
+def main(argv):
+
+    print subprocess.check_output(['bower','install'])
+
+    componentsfile.write('define(function(require) {\n');
+
+    for dependency in config['dependencies']:
+        componentConfig = json.loads(open(os.path.join(os.path.dirname(__file__), dependency+'/bower.json')).read())
+
+        if 'main' in componentConfig:
+            componentsfile.write("require('jsx!./"+dependency+"/"+componentConfig['main']+"');\n")
+
+    componentsfile.write('});');
+    componentsfile.close();
+
+if __name__ == "__main__":
+	main(sys.argv[1:])

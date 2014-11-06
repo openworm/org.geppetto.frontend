9221a61eea3ac3bfbef25da6a9ea2cd4640407bb
Adrian Quintana <adrian.perez@ucl.ac.uk>
2014-11-06 19:40:58 +0000

    Connectivity widget...first approach

--- src/main/webapp/js/widgets/connectivity/Connectivity.js
+++ src/main/webapp/js/widgets/connectivity/Connectivity.js
@@ -44,101 +44,328 @@ define(function(require) {
 
 	return Widget.View.extend({
 		
-			dataset: null,
-			data: {},
+		dataset: {},
+		
+		defaultConnectivityOptions:  {
+			width: 460,
+			height: 460,
+			connectivityLayout: "matrix", //[matrix, hive, force]
+		},
+		
+		initialize : function(options){
+			Widget.View.prototype.initialize.call(this,options);
 			
-			defaultConnectivityOptions:  {
-				width: 460,
-				height: 460,
-				connectivityLayout: "matrix", //[matrix, hive, force]
-			},
+			this.options = this.defaultConnectivityOptions;
 			
-			initialize : function(options){
-				Widget.View.prototype.initialize.call(this,options);
-				
-				this.options = this.defaultConnectivityOptions;
-
-				this.render();
-				this.setSize(options.width,options.height);
-				
-				
-	
-			},
 			
-			setData : function(root, options){
-				this.setOptions(options);
-	
-				this.dataset = root;
-				
-				this.data = [];
-				
-				if (this.dataset._metaType == "EntityNode"){
-					var subEntities = this.dataset.getEntities();
-					this.data["nodes"] = {};
-					this.data["links"] = [];
-					this.data["graph"] = new Array(1);
-					this.data["multigraph"] = false;
-					this.data["directed"] = true;
-					for (var subEntityIndex in subEntities){
-						var nodeItem = {};
-						nodeItem["id"] = subEntities[subEntityIndex].getId();
-						this.data["nodes"][nodeItem["id"]] = nodeItem;
-
-						var connections = subEntities[subEntityIndex].getConnections();
-						for (var connectionIndex in connections){
-							var linkItem = {};
-							
-							var connectionItem = connections[connectionIndex];
-							if (connectionItem.getType() == "FROM"){
-								linkItem["source"] = connectionItem.getParent().getId();
-								linkItem["target"] = connectionItem.getEntityInstancePath().substring(connectionItem.getEntityInstancePath().indexOf('.') + 1);
-//								var customNodes = connectionItem.getCustomNodes();
-//								
-//								for (var customNodeIndex in connectionItem.getCustomNodes()){
-//									var customNodesChildren = customNodes[customNodeIndex].getChildren();
-//									for (var customNodeChildIndex in customNodesChildren){
-//										if (customNodesChildren[customNodeChildIndex].getId() == "Id"){
-//											linkItem["synapse"] = customNodesChildren[customNodeChildIndex].getValue();
-//										}
-//									}
-//								}
-								
-							}
-							
-							this.data["links"].push(linkItem);
-						}
-					}
-					console.log(this.data);
+			this.render();
+			this.setSize(options.width,options.height);
+			
+			this.connectivityContainer = $("#" +this.id);
+			this.connectivityContainer.on("dialogresizestop", function(event, ui) {
+			    //TODO: To subtract 20px is horrible and has to be replaced but I have no idea about how to calculate it
+				var width = $(this).innerWidth()-20;
+				var height = $(this).innerHeight()-20;
+				if (window[this.id].options.connectivityLayout == 'force'){
+					window[this.id].svg.attr("width", width).attr("height", height);
+					window[this.id].force.size([width, height]).resume();
 				}
-				
-				return "Metadata or variables added to connectivity widget";
-			},
+//				else if (window[this.id].options.connectivityLayout == 'matrix') {
+//					window[this.id].rect.attr("width", width).attr("height", height);
+//				}
+				event.stopPropagation();
+		    });
+		},
+		
+		setData : function(root, options){
+			this.setOptions(options);
+
+			this.dataset = {};
+			this.mapping = {};
+			this.mappingSize = 0;
+			this.dataset["root"] = root;
 			
-			/**
-			 *
-			 * Set the options for the connectivity widget
-			 *
-			 * @command setOptions(options)
-			 * @param {Object} options - options to modify the plot widget
-			 */
-			setOptions: function(options) {
-				if(options != null) {
-					$.extend(this.options, options);
-				}
-			},
+			this.options.innerWidth = this.connectivityContainer.innerWidth()-20;
+			this.options.innerHeight = this.connectivityContainer.innerHeight()-20;
 			
-			/**
-			 * Sets the legend for a variable
-			 * 
-			 * @command setLegend(variable, legend)
-			 * @param {Object} variable - variable to change display label in legends
-			 * @param {String} legend - new legend name
-			 */
-			setLegend : function(variable, legend){
-				
+			this.svg = d3.select("#"+this.id).append("svg")
+            .attr("width", this.options.innerWidth)
+            .attr("height", this.options.innerHeight);
+			
+			this.createDataFromConnections();
+			
+			if (this.options.connectivityLayout == 'matrix'){
+				this.createMatrixLayout();
+			}
+			else if (this.options.connectivityLayout == 'force') {
+				this.createForceLayout();
 			}
 			
+			return "Metadata or variables added to connectivity widget";
+		},
+		
+		createDataFromConnections: function(){
+			if (this.dataset["root"]._metaType == "EntityNode"){
+				var subEntities = this.dataset["root"].getEntities();
+				
+				this.dataset["nodes"] = [];
+				this.dataset["links"] = [];
+//					this.dataset["graph"] = new Array(1);
+//					this.dataset["multigraph"] = false;
+//					this.dataset["directed"] = true;
+
+				for (var subEntityIndex in subEntities){
+					var connections = subEntities[subEntityIndex].getConnections();
+					for (var connectionIndex in connections){
+						var connectionItem = connections[connectionIndex];
+						if (connectionItem.getType() == "FROM"){
+							var source = connectionItem.getParent().getId();
+							var target = connectionItem.getEntityInstancePath().substring(connectionItem.getEntityInstancePath().indexOf('.') + 1);
+							
+							this.createNode(source);
+							this.createNode(target);
+							
+							var linkItem = {};
+							linkItem["source"] = this.mapping[source];
+							linkItem["target"] = this.mapping[target];
+							
+							var customNodes = connectionItem.getCustomNodes();
+							for (var customNodeIndex in connectionItem.getCustomNodes()){
+								var customNodesChildren = customNodes[customNodeIndex].getChildren().models;
+								for (var customNodeChildIndex in customNodesChildren){
+									if (customNodesChildren[customNodeChildIndex].getId() == "Id"){
+										linkItem["synapse_type"] = customNodesChildren[customNodeChildIndex].getValue();
+									}
+									else if (customNodesChildren[customNodeChildIndex].getId() == "GBase"){
+										linkItem["weight"] = customNodesChildren[customNodeChildIndex].getValue();
+									}
+								}
+							}
+							
+							this.dataset["links"].push(linkItem);
+						}
+						
+					}
+				}
+			}
+			console.log(this.dataset);
+		},
+		
+		createForceLayout: function(){
+			var ncolor = d3.scale.category10();
+		    var scolor = d3.scale.category20().domain(["Acetylcholine","Acetylcholine_Tyramine",
+		                                          "Dopamine","FMRFamide","GABA","Generic_GJ",
+		                                          "Glutamate", "Octopamine", "Serotonin",
+		                                          "Serotonin_Acetylcholine","Serotonin_Glutamate"]);
+		    this.force = d3.layout.force()
+		        .charge(-250)
+		        .linkDistance(60)
+		        .size([this.options.innerWidth, this.options.innerHeight]);
+
+		        
+		    this.force.nodes(this.dataset.nodes)
+	            .links(this.dataset.links)
+	            .start();
+
+	        var link = this.svg.selectAll(".link")
+	            .data(this.dataset.links)
+	            .enter().append("line")
+	            .attr("class", "link")
+	            .style("stroke", function(d) {return scolor(d.neurotransmitter);})
+	            .style("stroke-width", function(d) {return 0.5*d.weight;});
+
+	            
+	        var node = this.svg.selectAll(".node")
+	            .data(this.dataset.nodes)
+	            .enter().append("circle")
+	            .attr("class", "node")
+	            .attr("r", 5)  // radius
+	            .style("fill", function(d) {
+	                return ncolor(d.community); 
+	            })
+	            .call(this.force.drag);
+
+	        node.append("title")
+	            .text(function(d) { return d.id; });
+
+	        this.force.on("tick", function() {
+	            link.attr("x1", function(d) { return d.source.x; })
+	                .attr("y1", function(d) { return d.source.y; })
+	                .attr("x2", function(d) { return d.target.x; })
+	                .attr("y2", function(d) { return d.target.y; });
+
+	            node.attr("cx", function(d) { return d.x; })
+	                .attr("cy", function(d) { return d.y; });
+	        });
+		},
+		
+		createMatrixLayout: function(){
+			var x = d3.scale.ordinal().rangeBands([0, this.options.innerWidth]),
+	        z = d3.scale.linear().domain([0, 4]).clamp(true),
+	        //c = d3.scale.category10().domain(d3.range(10));
+	        c = d3.scale.ordinal().range(["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728"]);
+	    
+			var margin = {top: 80, right: 0, bottom: 10, left: 80};
 			
-	
-		});
+			this.svg
+			.attr("width", this.options.innerWidth + margin.left + margin.right)
+            .attr("height", this.options.innerHeight + margin.top + margin.bottom)
+			.style("margin-left", -margin.left + "px")
+			.append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		    var matrix = [];
+		    var nodes = this.dataset.nodes;
+		    var n = nodes.length;
+
+		    // Compute index per node.
+		    nodes.forEach(function(node, i) {
+		    	node.pre_count = 0;
+		        node.post_count = 0;
+		        matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
+		    });
+		    
+		    // Convert links to matrix; count pre / post conns.
+		    this.dataset.links.forEach(function(link) {
+		    	matrix[link.source][link.target].z = link.weight ? link.synapse_type : 0;
+		    	nodes[link.source].pre_count += 1;
+		    	nodes[link.target].post_count += 1;
+		    });
+
+		    // Precompute the orders.
+		    var orders = {
+		        id: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].id, nodes[b].id); }),
+		        pre_count: d3.range(n).sort(function(a, b) { return nodes[b].pre_count - nodes[a].pre_count; }),
+		        post_count: d3.range(n).sort(function(a, b) { return nodes[b].post_count - nodes[a].post_count; }),
+		    };
+
+		    // The default sort order.
+		    x.domain(orders.id);
+
+		    this.rect = this.svg.append("rect")
+		          .attr("class", "background")
+		          .attr("width", this.options.innerWidth)
+		          .attr("height", this.options.innerHeight);
+
+		        
+		    var row = this.svg.selectAll(".row")
+		          .data(matrix)
+		        .enter().append("g")
+		          .attr("class", "row")
+		          .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+		          .each(row);
+
+		      row.append("line")
+		          .attr("x2", this.options.innerWidth);
+
+		      row.append("text")
+		          .attr("x", -6)
+		          .attr("y", x.rangeBand() / 2)
+		          .attr("dy", ".32em")
+		          .attr("text-anchor", "end")
+		          .text(function(d, i) {
+		        	  return nodes[i].id; 
+		        	  });
+
+		      var column = this.svg.selectAll(".column")
+		          .data(matrix)
+		        .enter().append("g")
+		          .attr("class", "column")
+		          .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+
+		      column.append("line")
+		          .attr("x1", -this.options.innerWidth);
+
+		      column.append("text")
+		          .attr("x", 6)
+		          .attr("y", x.rangeBand() / 2)
+		          .attr("dy", ".32em")
+		          .attr("text-anchor", "start")
+		          .text(function(d, i) { 
+		        	  return nodes[i].id; 
+		        	  });
+
+		      function row(row) {
+		        var cell = d3.select(this).selectAll(".cell")
+		            .data(row.filter(function(d) { return d.z;})) //only paint conns
+		          .enter().append("rect")
+		            .attr("class", "cell")
+		            .attr("x", function(d) { return x(d.x); })
+		            .attr("width", x.rangeBand())
+		            .attr("height", x.rangeBand())
+		            .attr("title", function(d) { return d.id;})
+		            .style("fill-opacity", function(d) { return z(d.z); })
+		            .style("fill", function(d) {return c(d.z); });
+		            //.on("mouseover", mouseover)
+		            //.on("mouseout", mouseout);
+		            
+		      }
+
+		       function mouseover(p) {
+		         d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
+		         d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
+		        }
+
+		       function mouseout() {
+		         d3.selectAll("text").classed("active", false);
+		       }
+
+		      d3.select("#order").on("change", function() {
+		        order(this.value);
+		      });
+
+		      function order(value) {
+		        x.domain(orders[value]);
+
+		        var t = this.svg.transition().duration(2500);
+
+		        t.selectAll(".row")
+		            .delay(function(d, i) { return x(i) * 4; })
+		            .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+		          .selectAll(".cell")
+		            .delay(function(d) { return x(d.x) * 4; })
+		            .attr("x", function(d) { return x(d.x); });
+
+		        t.selectAll(".column")
+		            .delay(function(d, i) { return x(i) * 4; })
+		            .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+		      }
+		},
+		
+		createNode: function(nodeId) {
+			if (!(nodeId in this.mapping)){
+				var nodeItem = {};
+				nodeItem["id"] = nodeId;
+				this.dataset["nodes"].push(nodeItem);
+				
+				this.mapping[nodeItem["id"]] = this.mappingSize;
+				this.mappingSize++;
+			}
+		},
+		
+		/**
+		 *
+		 * Set the options for the connectivity widget
+		 *
+		 * @command setOptions(options)
+		 * @param {Object} options - options to modify the plot widget
+		 */
+		setOptions: function(options) {
+			if(options != null) {
+				$.extend(this.options, options);
+			}
+		},
+		
+		/**
+		 * Sets the legend for a variable
+		 * 
+		 * @command setLegend(variable, legend)
+		 * @param {Object} variable - variable to change display label in legends
+		 * @param {String} legend - new legend name
+		 */
+		setLegend : function(variable, legend){
+			
+		}
+		
+
+	});
 });

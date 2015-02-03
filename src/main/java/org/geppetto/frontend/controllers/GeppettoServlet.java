8e065167545a96f4946dd4cca4bbce59024cf112
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-02-03 13:06:00 +0000

    Copyright year update

--- src/main/java/org/geppetto/frontend/controllers/GeppettoServlet.java
+++ src/main/java/org/geppetto/frontend/controllers/GeppettoServlet.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * The MIT License (MIT)
  *
- * Copyright (c) 2011, 2013 OpenWorm.
+ * Copyright (c) 2011 - 2015 OpenWorm.
  * http://openworm.org
  *
  * All rights reserved. This program and the accompanying materials

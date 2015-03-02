ae9f1f2f211fb1f2286571dea166ec193821241f
Dan D, <dan@metacell.us>
2015-03-02 20:24:21 +0200

    Prepared the service infrastructure for the authentication.

--- src/main/java/org/geppetto/frontend/controllers/Application.java
+++ src/main/java/org/geppetto/frontend/controllers/Application.java
@@ -1,6 +1,8 @@
 package org.geppetto.frontend.controllers;
 
 import org.geppetto.core.auth.IAuthService;
+import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.frontend.server.AuthServiceCreator;
 import org.osgi.framework.BundleContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,30 +10,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class Application {
+public class Application
+{
 
-    @Autowired
-    BundleContext bundleContext;
+	@Autowired
+	BundleContext bundleContext;
 
-    @Autowired(required = false)
-    IAuthService authService;
+	// @Autowired(required = false)
+	IAuthService authService;
 
-    public Application() {
-    }
+	public Application()
+	{
+	}
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home()
+	{
+		try
+		{
+			authService = new AuthServiceCreator(IAuthService.class.getName()).getService();
+		}
+		catch(GeppettoInitializationException e)
+		{
+			// TODO: logging
+			e.printStackTrace();
+		}
+		if(authService == null || authService.isAuthenticated())
+		{
+			return "dist/index";
+		}
+		else
+		{
+			return "redirect:" + authService.authFailureRedirect();
+		}
+	}
 
-        if (authService == null || authService.isAuthenticated()) {
-            return "dist/index";
-        } else {
-            return "redirect:" + authService.authFailureRedirect();
-        }
-    }
-
-    @RequestMapping(value = "/GeppettoTests.html", method = RequestMethod.GET)
-    public String test() {
-        return "dist/geppettotests";
-    }
+	@RequestMapping(value = "/GeppettoTests.html", method = RequestMethod.GET)
+	public String test()
+	{
+		return "dist/geppettotests";
+	}
 
 }

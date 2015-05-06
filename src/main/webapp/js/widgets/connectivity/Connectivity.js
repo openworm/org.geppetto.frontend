92e3e07980dc5fc9eba4bd35964c85368e43c675
borismarin <borismarin@gmail.com>
2015-05-06 11:08:41 +0100

    Minor reorganizatin / comments.

--- src/main/webapp/js/widgets/connectivity/Connectivity.js
+++ src/main/webapp/js/widgets/connectivity/Connectivity.js
@@ -34,7 +34,7 @@
  * Tree Visualiser Widget
  *
  * @author Adrian Quintana (adrian.perez@ucl.ac.uk)
- * @author Boris Marin 
+ * @author borismarin
  */
 
 define(function(require) {
@@ -74,6 +74,7 @@ define(function(require) {
 					this.force.size([width, height]).resume();
 				}
 				else if (this.options.connectivityLayout == 'matrix') {
+					$('#' + this.id + '-ordering').remove();
 					this.createLayout();
 				}
 			}
@@ -144,12 +145,11 @@ define(function(require) {
             this.options.innerHeight = this.connectivityContainer.innerHeight() - this.widgetMargin;
             
             this.svg = d3.select("#"+this.id)
-                            .append("svg")
+                          .append("svg")
                             .attr("width", this.options.innerWidth)
                             .attr("height", this.options.innerHeight);
             
             if (this.options.connectivityLayout == 'matrix'){
-                $('#' + this.id + '-filters').remove();
                 this.createMatrixLayout();
             }
             else if (this.options.connectivityLayout == 'force') {
@@ -221,6 +221,159 @@ define(function(require) {
         },
 
 
+        createMatrixLayout: function(){
+
+            var margin = {top: 30, right: 10, bottom: 10, left: 3};
+            var legendWidth = 120;
+
+            var matrixDim = (this.options.innerHeight < (this.options.innerWidth - legendWidth))?(this.options.innerHeight):(this.options.innerWidth - legendWidth);
+            
+            var x = d3.scale.ordinal().rangeBands([0, matrixDim - margin.top]),
+            // Opacity
+            z = d3.scale.linear().domain([0, 4]).clamp(true),
+            // Colors
+            c = d3.scale.category10();
+            
+            this.svg
+                .style("padding-left", margin.left + "px")
+                .style("padding-top", margin.top + "px")
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var matrix = [];
+            var nodes = this.dataset.nodes;
+            var root=this.dataset.root;
+            var n = nodes.length;
+
+            // Compute index per node.
+            nodes.forEach(function(node, i) {
+                node.pre_count = 0;
+                node.post_count = 0;
+                matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
+            });
+            
+            // Convert links to matrix; count pre / post conns.
+            this.dataset.links.forEach(function(link) {
+                matrix[link.source][link.target].z = link.weight ? link.linkType : 0;
+                nodes[link.source].pre_count += 1;
+                nodes[link.target].post_count += 1;
+            });
+
+            //Sorting matrix entries.
+            //TODO: runtime specified sorting criteria
+        	var sortOptions = {
+        			'id' : 'By entity name',
+        			'pre_count' : 'By # pre',
+        			'post_count' : 'By # post'
+        	};
+            //  Precompute the orders.
+            var orders = {
+                id: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].id, nodes[b].id); }),
+                pre_count: d3.range(n).sort(function(a, b) { return nodes[b].pre_count - nodes[a].pre_count; }),
+                post_count: d3.range(n).sort(function(a, b) { return nodes[b].post_count - nodes[a].post_count; }),
+                //community: d3.range(n).sort(function(a, b) { return nodes[b].community - nodes[a].community; }),
+            };
+            // Default sort order.
+            x.domain(orders.id);
+            
+            var rect = this.svg
+                          .append("rect")
+                            .attr("class", "background")
+                            .attr("width", matrixDim - margin.left)
+                            .attr("height", matrixDim - margin.top);
+
+            var row = this.svg.selectAll(".row")
+                  .data(matrix)
+                .enter().append("g")
+                  .attr("class", "row")
+                  .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+                  .each(row);
+
+            row.append("line")
+                  .attr("x2", this.options.innerWidth);
+
+            var column = this.svg.selectAll(".column")
+                  .data(matrix)
+                .enter().append("g")
+                  .attr("class", "column")
+                  .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+
+            column.append("line")
+                    .attr("x1", -this.options.innerWidth);
+              
+            var tooltip = this.svg
+                .append("text")
+                  .attr("x", 0)
+                  .attr("y", -10)
+                  .attr('class', 'connectionlabel')
+                  .text("Hover the squares to see the connections.");
+            
+           this.createLegend('legend', c, {x: matrixDim, y:0});
+            
+           //Sorting matrix entries by criteria specified via combobox 
+		    var orderContainer = $('<div/>', {
+		    	id: this.id + '-ordering',
+		    	style: 'width:' + legendWidth + 'px;left:' + (matrixDim + this.widgetMargin) + 'px;top:' + (matrixDim - 32) + 'px;',
+		    	class: 'connectivity-ordering'
+		    }).appendTo(this.connectivityContainer);
+		    
+		    var orderCombo = $('<select/>');
+		    $.each(sortOptions, function(k, v) {
+		        $('<option/>', {value: k, text: v}).appendTo(orderCombo);
+		    });
+		    orderContainer.append($('<span/>', {class: 'connectivity-ordering-label', text: 'Order by:'}).append(orderCombo))
+               
+            orderCombo.on("change", function(svg) {
+                return function() {
+                    x.domain(orders[this.value]);
+            
+                    var t = svg.transition().duration(2500);
+                    t.selectAll(".row")
+                        .delay(function(d, i) { return x(i) * 4; })
+                        .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
+                        .selectAll(".cell")
+                        .delay(function(d) { return x(d.x) * 4; })
+                        .attr("x", function(d) { return x(d.x); });
+            
+                    t.selectAll(".column")
+                        .delay(function(d, i) { return x(i) * 4; })
+                        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
+                }
+            }(this.svg));
+            
+            // Draw squares for each connection
+            function row(row) {
+                var cell = d3.select(this).selectAll(".cell")
+                    .data(row.filter(function(d) { return d.z;})) //only paint conns
+                  .enter().append("rect")
+                    .attr("class", "cell")
+                    .attr("x", function(d) { return x(d.x); })
+                    .attr("width", x.rangeBand())
+                    .attr("height", x.rangeBand())
+                    .attr("title", function(d) { return d.id;})
+                    .style("fill-opacity", function(d) { return z(d.z); })
+                    .style("fill", function(d) {return c(d.z); })
+                    .on("click", function(d){
+                        Simulation.unSelectAll();
+                        //Ideally instead of hiding the connectivity lines we'd show only the ones connecting the two cells, also we could higlight the connection.
+                        eval(root.name+"."+nodes[d.x].id).select();
+                        eval(root.name+"."+nodes[d.x].id).showConnectionLines(false);
+                        eval(root.name+"."+nodes[d.y].id).select();
+                        eval(root.name+"."+nodes[d.y].id).showConnectionLines(false);
+                        })
+                    .on("mouseover", function(d){
+                        d3.select(this.parentNode.appendChild(this)).transition().duration(100).style({'stroke-opacity':1,'stroke':'white', 'stroke-width':'2'});
+                        d3.select("body").style('cursor','pointer');
+                        return tooltip.transition().duration(100).text(nodes[d.x].id + " is connected to " + nodes[d.y].id);
+                        })
+                    .on("mouseout", function(){
+                        d3.select(this).transition().duration(100).style({'stroke-opacity':0,'stroke':'white'});
+                        d3.select("body").style('cursor','default');
+                        return tooltip.text("");
+                        });
+            }
+        },
+
+
         createLegend: function(id, colorScale, position, title){
 
         	//TODO: boxes should scale based on number of items 
@@ -267,174 +420,6 @@ define(function(require) {
         	
         },
 
-
-        createMatrixLayout: function(){
-
-            var margin = {top: 30, right: 10, bottom: 10, left: 3};
-
-            var legendWidth = 120;
-
-            var matrixDim = (this.options.innerHeight < (this.options.innerWidth - legendWidth))?(this.options.innerHeight):(this.options.innerWidth - legendWidth);
-            
-            var x = d3.scale.ordinal().rangeBands([0, matrixDim - margin.top]),
-            // Opacity
-            z = d3.scale.linear().domain([0, 4]).clamp(true),
-            // Colors
-            c = d3.scale.category10();
-            
-            this.svg
-                .style("padding-left", margin.left + "px")
-                .style("padding-top", margin.top + "px")
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-            var matrix = [];
-            var nodes = this.dataset.nodes;
-            var root=this.dataset.root;
-            var n = nodes.length;
-
-            // Compute index per node.
-            nodes.forEach(function(node, i) {
-                node.pre_count = 0;
-                node.post_count = 0;
-                matrix[i] = d3.range(n).map(function(j) { return {x: j, y: i, z: 0}; });
-            });
-            
-            // Convert links to matrix; count pre / post conns.
-            this.dataset.links.forEach(function(link) {
-                matrix[link.source][link.target].z = link.weight ? link.linkType : 0;
-                nodes[link.source].pre_count += 1;
-                nodes[link.target].post_count += 1;
-            });
-
-            // Precompute the orders.
-            var orders = {
-                id: d3.range(n).sort(function(a, b) { return d3.ascending(nodes[a].id, nodes[b].id); }),
-                //community: d3.range(n).sort(function(a, b) { return nodes[b].community - nodes[a].community; }),
-                pre_count: d3.range(n).sort(function(a, b) { return nodes[b].pre_count - nodes[a].pre_count; }),
-                post_count: d3.range(n).sort(function(a, b) { return nodes[b].post_count - nodes[a].post_count; }),
-            };
-            
-//          TODO: Commented it out until we have Louvain Community detection was implemented
-//          var colours = {
-//                  id: function(d) {return c(d.z);},
-//                  community: function(d) {return nodes[d.x].community == nodes[d.y].community ? c(nodes[d.x].community) : null;},
-//          };
-
-            // The default sort order.
-            x.domain(orders.id);
-            
-            var rect = this.svg
-                          .append("rect")
-                            .attr("class", "background")
-                            .attr("width", matrixDim - margin.left)
-                            .attr("height", matrixDim - margin.top);
-
-                
-            var row = this.svg.selectAll(".row")
-                  .data(matrix)
-                .enter().append("g")
-                  .attr("class", "row")
-                  .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-                  .each(row);
-
-            row.append("line")
-                  .attr("x2", this.options.innerWidth);
-
-            var column = this.svg.selectAll(".column")
-                  .data(matrix)
-                .enter().append("g")
-                  .attr("class", "column")
-                  .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-
-            column.append("line")
-                    .attr("x1", -this.options.innerWidth);
-              
-            var tooltip = this.svg
-                .append("text")
-                  .attr("x", 0)
-                  .attr("y", -10)
-                  .attr('class', 'connectionlabel')
-                  .text("Hover the squares to see the connections.");
-            
-           this.createLegend('legend', c, {x: matrixDim, y:0});
-            
-            //FILTERS AND EVENTS
-		    //this.connectivityContainer.append("<div id='filters' style='width:" + legendWidth + "px;left:" + (matrixDim + this.widgetMargin) + "px;top:" + (matrixDim - 32) + "px;'></div>");
-		    var orderContainer = $('<div/>', {
-		    	id: this.id + '-ordering',
-		    	style: 'width:' + legendWidth + 'px;left:' + (matrixDim + this.widgetMargin) + 'px;top:' + (matrixDim - 32) + 'px;',
-		    	class: 'connectivity-ordering'
-		    }).appendTo(this.connectivityContainer);
-		    
-		    var orderCombo = $('<select/>');
-		    var sortOptions = {'id': 'By entity name',
-		    				'pre_count': 'By # pre',
-		    				'post_count': 'By # post'};
-		    $.each(sortOptions, function(k, v) {
-		        $('<option/>', {value: k, text: v}).appendTo(orderCombo);
-		    });
-		    orderContainer.append($('<span/>', {class: 'connectivity-ordering-label', text: 'Order by:'}).append(orderCombo))
-               
-            orderCombo.on("change", function(svg) {
-                return function() {
-                    x.domain(orders[this.value]);
-            
-                    var t = svg.transition().duration(2500);
-                    t.selectAll(".row")
-                        .delay(function(d, i) { return x(i) * 4; })
-                        .attr("transform", function(d, i) { return "translate(0," + x(i) + ")"; })
-                        .selectAll(".cell")
-                        .delay(function(d) { return x(d.x) * 4; })
-                        .attr("x", function(d) { return x(d.x); });
-            
-                    t.selectAll(".column")
-                        .delay(function(d, i) { return x(i) * 4; })
-                        .attr("transform", function(d, i) { return "translate(" + x(i) + ")rotate(-90)"; });
-                }
-            }(this.svg));
-            
-            // UTILITIES
-            function row(row) {
-                var cell = d3.select(this).selectAll(".cell")
-                    .data(row.filter(function(d) { return d.z;})) //only paint conns
-                    .enter().append("rect")
-                    .attr("class", "cell")
-                    .attr("x", function(d) { return x(d.x); })
-                    .attr("width", x.rangeBand())
-                    .attr("height", x.rangeBand())
-                    .attr("title", function(d) { return d.id;})
-                    .style("fill-opacity", function(d) { return z(d.z); })
-                    .style("fill", function(d) {return c(d.z); })
-                    .on("click", function(d){
-                        Simulation.unSelectAll();
-                        //Ideally instead of hiding the connectivity lines we'd show only the ones connecting the two cells, also we could higlight the connection.
-                        eval(root.name+"."+nodes[d.x].id).select();
-                        eval(root.name+"."+nodes[d.x].id).showConnectionLines(false);
-                        eval(root.name+"."+nodes[d.y].id).select();
-                        eval(root.name+"."+nodes[d.y].id).showConnectionLines(false);
-                        })
-                    .on("mouseover", function(d){
-                        d3.select(this.parentNode.appendChild(this)).transition().duration(100).style({'stroke-opacity':1,'stroke':'white', 'stroke-width':'2'});
-                        d3.select("body").style('cursor','pointer');
-                        return tooltip.transition().duration(100).text(nodes[d.x].id + " is connected to " + nodes[d.y].id);
-                        })
-                    .on("mouseout", function(){
-                        d3.select(this).transition().duration(100).style({'stroke-opacity':0,'stroke':'white'});
-                        d3.select("body").style('cursor','default');
-                        return tooltip.text("");
-                        });
-            }
-            
-            
-            function mouseover(p) {
-                d3.selectAll(".row text").classed("active", function(d, i) { return i == p.y; });
-                d3.selectAll(".column text").classed("active", function(d, i) { return i == p.x; });
-            }
-            
-            function mouseout() {
-                d3.selectAll("text").classed("active", false);
-            }
-        },
         
         createNode: function(nodeId) {
             if (!(nodeId in this.mapping)){

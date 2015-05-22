791d1394302e910a067c68a0a3b8d17c19db7956
johnidol <giovanni.idili@gmail.com>
2015-05-22 20:29:13 +0100

    animate skinned meshes when loading colladas

--- src/main/webapp/js/3d_visualization/GEPPETTO.SceneFactory.js
+++ src/main/webapp/js/3d_visualization/GEPPETTO.SceneFactory.js
@@ -96,7 +96,7 @@ define(function(require) {
 												metaType == "CylinderNode") {
 											GEPPETTO.SceneFactory.updateGeometry(node);								
 									}
-									else if (metaType == "SkeletonAnimationNode" && GEPPETTO.getVARS().customRendererClass != null) {
+									else if (metaType == "SkeletonAnimationNode") {
 										// get transformation matrices
 										var matrices = node.skeletonTransformations;
 										
@@ -285,8 +285,12 @@ define(function(require) {
 						scene = collada.scene;
 						scene.traverse(function(child){
 							if(child instanceof THREE.Mesh){
-								child.material =GEPPETTO.SceneFactory.getMeshPhongMaterial();
+								child.material = GEPPETTO.SceneFactory.getMeshPhongMaterial();
 								child.name = node.instancePath.split(".VisualizationTree")[0];
+							}
+							if ( child instanceof THREE.SkinnedMesh ) {
+								var animation = new THREE.Animation( child, child.geometry.animation );
+								animation.play();
 							}
 						});
 					});

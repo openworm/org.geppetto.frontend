0fbf36703175514ef10f70563e3e34c5249da2c2
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-06-12 18:23:24 +0100

    Commenting out welcome dialog for now. It will have to be moved to the dashboard to still be meaningful but not clear if it will still be adding value at this point.

--- src/main/webapp/js/components/components.js
+++ src/main/webapp/js/components/components.js
@@ -1,7 +1,7 @@
 define(function(require) {
 require('jsx!./dev/simulationcontrols/ExperimentControls');
 require('jsx!./dev/cameracontrols/CameraControls');
-require('jsx!./dev/tutorial/IntroModal');
+//require('jsx!./dev/tutorial/IntroModal');
 require('jsx!./dev/logo/logo');
 require('jsx!./dev/share/share');
 require('jsx!./dev/contact/contact');

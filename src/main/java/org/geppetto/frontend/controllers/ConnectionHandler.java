b6e8e1a2ac433545f85f73534e79b907c565ca1c
jrmartin <yeshuarmartinez@gmail.com>
2015-06-30 16:26:45 +0100

    adding more tests for qunit

--- src/main/java/org/geppetto/frontend/controllers/ConnectionHandler.java
+++ src/main/java/org/geppetto/frontend/controllers/ConnectionHandler.java
@@ -774,7 +774,9 @@ public class ConnectionHandler implements IGeppettoManagerCallbackListener
 	private void error(Exception e, String errorMessage)
 	{
 		String jsonExceptionMsg = "";
-		jsonExceptionMsg = e.getCause() == null ? e.getMessage() : e.toString();
+		if(e!=null){
+			jsonExceptionMsg = e.getCause() == null ? e.getMessage() : e.toString();
+		}
 		String jsonErrorMsg = errorMessage == null ? "" : errorMessage;
 		String error = "{ \"error_code\": \"" + GeppettoErrorCodes.GENERIC + "\", \"message\": \"" + jsonErrorMsg + "\", \"exception\": \"" + jsonExceptionMsg + "\"}";
 		logger.error(errorMessage, e);

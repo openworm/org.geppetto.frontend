5117926bc85097e393a58867681bf861972d6dc7
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2015-11-25 11:31:17 +0000

    Fixes for math.js in QUnit tests

--- src/main/webapp/js/GeppettoNeuronalSimulationTests.js
+++ src/main/webapp/js/GeppettoNeuronalSimulationTests.js
@@ -61,7 +61,9 @@ require.config({
 		jsx: 'vendor/jsx',
 	    JSXTransformer: 'vendor/JSXTransformer',
 	    text: 'vendor/text',
-		pako: 'vendor/pako.min'
+		pako: 'vendor/pako.min',
+		mathjs: 'vendor/math.min'
+
 	},
 	/*
 	 * Notes what dependencies are needed prior to loading each library, values on the right
@@ -121,6 +123,7 @@ jqueryLib.push("vendor/Detector");
 jqueryLib.push("vendor/jquery.cookie");
 jqueryLib.push("vendor/rAF");
 jqueryLib.push("pako");
+jqueryLib.push("mathjs");
 
 require(jqueryLib, function($) {
 

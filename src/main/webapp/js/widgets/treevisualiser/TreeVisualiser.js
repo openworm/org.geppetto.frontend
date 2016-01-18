c9f856919d33b0448ed574aaac093cea9dd2c5b3
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-01-18 19:50:12 +0000

    working on treevisualiser

--- src/main/webapp/js/widgets/treevisualiser/TreeVisualiser.js
+++ src/main/webapp/js/widgets/treevisualiser/TreeVisualiser.js
@@ -77,11 +77,81 @@ define(function(require) {
 				return {data:stateTreeVisualiserNode, isDisplayed:false};
 			},
 			
-			createTreeVisualiserNode: function(state){
-				var tvOptions = {wrappedObj : state};
+			createTreeVisualiserNode: function(state, formattedValue){
+				var tvOptions = {wrappedObj : state, formattedValue : formattedValue};
 				var tvn = new TreeVisualiserNode(tvOptions);
 				tvn.set({ "children" : this.createTreeVisualiserNodeChildren(state)});
 				return tvn;
+			},
+			
+			convertVariableToTreeVisualiserNode: function(node){
+				if (typeof node.getTypes != "undefined" && node.getTypes().length == 1){
+					var formattedValue = ""; 
+					switch (node.getTypes()[0].getMetaType()){
+						case GEPPETTO.Resources.PARAMETER_TYPE:
+							formattedValue = node.getInitialValues()[0].value.value + " " + node.getInitialValues()[0].value.unit.unit;
+							return this.createTreeVisualiserNode(node, formattedValue);
+							break;
+						case GEPPETTO.Resources.STATE_VARIABLE_TYPE:
+							formattedValue = node.getInitialValues()[0].value.value + " " + node.getInitialValues()[0].value.unit.unit;
+							return this.createTreeVisualiserNode(node, formattedValue);
+							break;
+						case GEPPETTO.Resources.CONNECTION_TYPE:
+							formattedValue = 'Connection';
+							return this.createTreeVisualiserNode(node, formattedValue);
+							break;
+						case GEPPETTO.Resources.DYNAMICS_TYPE:
+							formattedValue = node.getInitialValues()[0].value.dynamics.expression.expression;
+							return this.createTreeVisualiserNode(node, formattedValue);
+							break;
+						case GEPPETTO.Resources.FUNCTION_TYPE:
+							return this.createTreeVisualiserNode(node);
+							break;
+						case GEPPETTO.Resources.TEXT_TYPE:
+							formattedValue = node.getInitialValues()[0].value.text;
+							return this.createTreeVisualiserNode(node, formattedValue);
+							break;
+						case GEPPETTO.Resources.HTML_TYPE:
+							//children.push(this.createTreeVisualiserNode(child));
+							break;
+						case GEPPETTO.Resources.COMPOSITE_TYPE_NODE:
+							//AQP: This should be changed with supertype
+							if (node.getTypes()[0].getName().startsWith("projection -")){
+								var projectionChildren = node.getTypes()[0].getChildren();
+								var numConnections = 0;
+								var projectionsChildrenNode = [];
+								for (var j=0; j<projectionChildren.length; j++){
+									//AQP: This should be changed with supertype
+									if (projectionChildren[j].getTypes()[0].getName().startsWith("connection -")){
+										numConnections++;
+									}
+									else{
+										projectionsChildrenNode.push(this.convertVariableToTreeVisualiserNode(projectionChildren[j]));
+									}
+								}
+								
+								var treeVisualiserWrappedObject = new TreeVisualiserWrappedObject({"name": "Number of Connections", "id": "numberConnections", "_metaType": ""});
+								var tvOptions = {wrappedObj : treeVisualiserWrappedObject, formattedValue : numConnections};
+								var tvn = new TreeVisualiserNode(tvOptions);
+								projectionsChildrenNode.push(tvn);
+								
+								var projtvn = new TreeVisualiserNode({wrappedObj : node.getTypes()[0]});
+								projtvn.set({ "children" : projectionsChildrenNode});
+								return projtvn;
+							}
+							else{
+								return this.createTreeVisualiserNode(node.getTypes()[0]);	
+							}	
+							break;
+							
+						default:
+							//formattedValue = child.getInitialValues()[0].value.value;	
+							return this.createTreeVisualiserNode(node.getTypes()[0]);
+					}
+				}
+				if (typeof node.getAnonymousTypes != "undefined" && node.getAnonymousTypes().length == 1){
+					return this.createTreeVisualiserNode(node.getAnonymousTypes()[0]);	
+				}
 			},
 			
 			createTreeVisualiserNodeChildren: function(state){
@@ -89,32 +159,20 @@ define(function(require) {
 				if (state.getMetaType() == GEPPETTO.Resources.COMPOSITE_TYPE_NODE){
 					for (var i=0; i<state.getChildren().length; i++){
 						var child = state.getChildren()[i];
-						if (typeof child.getTypes != "undefined" && child.getTypes().length == 1){
-							if (child.getTypes()[0].getMetaType() == GEPPETTO.Resources.PARAMETER_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.STATE_VARIABLE_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.CONNECTION_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.DYNAMICS_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.FUNCTION_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.TEXT_TYPE ||
-									child.getTypes()[0].getMetaType() == GEPPETTO.Resources.HTML_TYPE){
-								children.push(this.createTreeVisualiserNode(child));
-							}
-							else{
-								children.push(this.createTreeVisualiserNode(child.getTypes()[0]));
-								
-							}
-
-						}
-						if (typeof child.getAnonymousTypes != "undefined" && child.getAnonymousTypes().length == 1){
-							children.push(this.createTreeVisualiserNode(child.getAnonymousTypes()[0]));	
-						}
+						var node = this.convertVariableToTreeVisualiserNode(child);
+						if (node != undefined)
+							children.push(node);
 					}
 				}
 				else if (state.getMetaType() == GEPPETTO.Resources.ARRAY_TYPE_NODE){
-					var treeVisualiserWrappedObject = new TreeVisualiserWrappedObject({"name": "size", "id": "size", "_metaType": "", "value": state.getSize()});
-					var tvOptions = {wrappedObj : treeVisualiserWrappedObject};
+					
+					// Size
+					var treeVisualiserWrappedObject = new TreeVisualiserWrappedObject({"name": "size", "id": "size", "_metaType": ""});
+					var tvOptions = {wrappedObj : treeVisualiserWrappedObject, formattedValue : state.getSize()};
 					var tvn = new TreeVisualiserNode(tvOptions);
 					children.push(tvn);
+					
+					// Array Type
 					children.push(this.createTreeVisualiserNode(state.getType()));
 					
 					//state.getDefaultValue()

42bca2df0a1d73c2b9d6275072bdf5d84061ca09
johnidol <giovanni.idili@gmail.com>
2016-01-20 12:46:10 +0000

    pointer and pointer element object created

---
+++ src/main/webapp/js/model/PointerElement.js
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2011, 2013 OpenWorm.
+ * http://openworm.org
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *
+ * Contributors:
+ *      OpenWorm - http://openworm.org/people.html
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+
+/**
+ * Client class use to represent a simple type.
+ *
+ * @module model/PointerElement
+ * @author Giovanni Idili
+ */
+define(function (require) {
+    var ObjectWrapper = require('model/ObjectWrapper');
+
+    return ObjectWrapper.Model.extend({
+        variable: null,
+        type: null,
+        index: null,
+
+        /**
+         * Initializes this node with passed attributes
+         *
+         * @param {Object} options - Object with options attributes to initialize node
+         */
+        initialize: function (options) {
+            this.set({"wrappedObj": options.wrappedObj});
+            this.set({"variable": options.variable});
+            this.set({"type": options.type});
+            this.set({"index": options.index});
+        },
+
+        /**
+         * Gets the variable
+         *
+         * @command Pointer.getVariable()
+         *
+         * @returns {Variable} - variable
+         *
+         */
+        getVariable: function () {
+            return this.get('variable');
+        },
+
+        /**
+         * Gets the type
+         *
+         * @command Pointer.getType()
+         *
+         * @returns {Type} - type
+         *
+         */
+        getType: function () {
+            return this.get('type');
+        },
+
+        /**
+         * Gets the index if it's pointing to an array element
+         *
+         * @command Pointer.getIndex()
+         *
+         * @returns {Integer} - index in a given array
+         *
+         */
+        getIndex: function () {
+            return this.get('index');
+        }
+    });
+});

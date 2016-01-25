a270150c69f19c69cb3487e9373b82e43eb8f98a
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-01-25 15:57:40 +0000

    Fix plotting bug with labels on mouseout

--- src/main/webapp/js/widgets/plot/Plot.js
+++ src/main/webapp/js/widgets/plot/Plot.js
@@ -174,14 +174,14 @@ define(function (require) {
                     } else {
                         y = p1[1] + (p2[1] - p1[1]) * (pos.x - p1[0]) / (p2[0] - p1[0]);
                     }
-                    var shortLabel = $("#" + this.id + " div.legendLabel").attr("shortLabel");
-                    $("#" + this.id + " div.legendLabel").eq(i).text(shortLabel + " = " + y.toFixed(3) + " @(" + pos.x.toFixed(3) + ")");
+                    var shortLabel = $("#" + this.id + " div.legendLabel[id='" + dataSet[i].label + "']").attr("shortLabel");
+                    $("#" + this.id + " div.legendLabel[id='" + dataSet[i].label + "']").text(shortLabel + " = " + y.toFixed(3) + " @(" + pos.x.toFixed(3) + ")");
                 }
             }
             else {
                 for (i = 0; i < dataSet.length; ++i) {
-                    var shortLabel = $("#" + this.id + " div.legendLabel").attr("shortLabel");
-                    $("#" + this.id + " div.legendLabel").eq(i).text(shortLabel);
+                    var shortLabel = $("#" + this.id + " div.legendLabel[id='" + dataSet[i].label + "']").attr("shortLabel");
+                    $("#" + this.id + " div.legendLabel[id='" + dataSet[i].label + "']").text(shortLabel);
                 }
             }
         },

894666d414c9894defa3787aceffde07885803c9
johnidol <giovanni.idili@gmail.com>
2016-02-09 16:37:00 +0000

    fixing more old tests

--- src/main/webapp/js/tests/QunitNeuronalTests.js
+++ src/main/webapp/js/tests/QunitNeuronalTests.js
@@ -38,7 +38,7 @@ define(function (require) {
      */
     function launch() {
         //start qunit tests
-        // start();
+        //start();
         //close socket
         GEPPETTO.MessageSocket.close();
         //clear message handlers, all tests within module should have performed by time method it's called
@@ -116,67 +116,90 @@ define(function (require) {
         });
 
         QUnit.module("Test Play Experiment");
-        QUnit.asyncTest("Load Project 1 - SingleComponentHH", function ( assert ) {
-            var initializationTime;
+        QUnit.test("Load Project 1 - SingleComponentHH", function ( assert ) {
+
+            var done = assert.async();
+
             var handler = {
                 onMessage: function (parsedServerMessage) {
                     // Switch based on parsed incoming message type
                     switch (parsedServerMessage.type) {
                         //Simulation has been loaded and model need to be loaded
                         case GEPPETTO.SimulationHandler.MESSAGE_TYPE.PROJECT_LOADED:
-                            //delete hhcell;
-                            var time = (new Date() - initializationTime) / 1000;
                             GEPPETTO.SimulationHandler.loadProject(JSON.parse(parsedServerMessage.data));
                             equal(window.Project.getId(), 1, "Project ID checked");
                             break;
+                        case GEPPETTO.SimulationHandler.MESSAGE_TYPE.MODEL_LOADED:
+                            GEPPETTO.SimulationHandler.loadModel(JSON.parse(parsedServerMessage.data));
+
+                            // test that geppetto model high level is as expected
+                            assert.ok(window.Model != undefined, "Model is not undefined");
+                            assert.ok(window.Model.getVariables() != undefined && window.Model.getVariables().length == 2 &&
+                                window.Model.getVariables()[0].getId() == 'hhcell' && window.Model.getVariables()[1].getId() == 'time',  "2 Variables as expected");
+                            assert.ok(window.Model.getLibraries() != undefined && window.Model.getLibraries().length == 2, "2 Libraries as expected");
+                            // test that instance tree high level is as expected
+                            assert.ok(window.Instances != undefined && window.Instances.length == 1 && window.Instances[0].getId() == 'hhcell', "1 top level instance as expected");
+
+                            break;
                         case GEPPETTO.SimulationHandler.MESSAGE_TYPE.EXPERIMENT_LOADED:
-                            var time = (new Date() - initializationTime) / 1000;
                             var payload = JSON.parse(parsedServerMessage.data);
                             GEPPETTO.SimulationHandler.loadExperiment(payload);
-                            //if experiment isn't completed don't play
+                            // if experiment isn't completed don't play
                             if (Project.getActiveExperiment().getStatus() != GEPPETTO.Resources.ExperimentStatus.COMPLETED) {
-                                ok(false, "Unable to play experiment for project");
+                                assert.ok(false, "Unable to play experiment for project");
+
+                                done();
                                 launch();
                             } else {
-                                Project.getActiveExperiment().play({step: 1});
+                                Project.getActiveExperiment().play();
                             }
                             break;
                         case GEPPETTO.SimulationHandler.MESSAGE_TYPE.PLAY_EXPERIMENT:
                             var payload = JSON.parse(parsedServerMessage.data);
-                            var timeSeries =
-                                hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q.getTimeSeries();
-                            equal(timeSeries.length, 0, "Checking updated time series in variable");
-                            GEPPETTO.SimulationHandler.playExperimentReady(payload);
-                            timeSeries =
-                                hhcell.electrical.SimulationTree.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q.getTimeSeries();
-                            equal(timeSeries.length, 6001, "Checking updated time series in variable");
+                            var timeSeries = hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q.getTimeSeries();
+                            assert.equal(timeSeries, null, "Checking that time series is still null in variable");
+
+                            var experimentState = JSON.parse(payload.update);
+                            var experiment = window.Project.getActiveExperiment();
+                            GEPPETTO.ExperimentsController.updateExperiment(experiment, experimentState);
+
+                            timeSeries = hhcell.hhpop[0].bioPhys1.membraneProperties.naChans.na.h.q.getTimeSeries();
+                            assert.equal(timeSeries.length, 6001, "Checking updated time series in variable");
+
+                            done();
                             launch();
                             break;
                         case GEPPETTO.GlobalHandler.MESSAGE_TYPE.INFO_MESSAGE:
                             var payload = JSON.parse(parsedServerMessage.data);
                             var message = JSON.parse(payload.message);
                             ok(false, message);
+
+                            done();
                             launch();
                             break;
                         case GEPPETTO.GlobalHandler.MESSAGE_TYPE.ERROR:
                             var payload = JSON.parse(parsedServerMessage.data);
                             var message = JSON.parse(payload.message).message;
                             ok(false, message);
+
+                            done();
                             launch();
                             break;
                         case GEPPETTO.GlobalHandler.MESSAGE_TYPE.ERROR_LOADING_PROJECT:
                             var payload = JSON.parse(parsedServerMessage.data);
                             var message = payload.message;
                             ok(false, message);
+
+                            done();
                             launch();
                             break;
                     }
                 }
             };
+
             GEPPETTO.MessageSocket.clearHandlers();
             GEPPETTO.MessageSocket.addHandler(handler);
             window.Project.loadFromID("1", "1");
-            initializationTime = new Date();
         });
 
         QUnit.module("Test Muscle cell NEURON simulation");

e019e0e6c2e1e7b86e12ad979c9fe694b6b4689c
borismarin <borismarin@gmail.com>
2016-07-05 19:41:08 +0100

    cleaning up button form editor

--- src/main/webapp/js/widgets/form/vendor/editors/Button.js
+++ src/main/webapp/js/widgets/form/vendor/editors/Button.js
@@ -1,98 +1,84 @@
 /**
- * Radio editor
+ * Button bar selector for selection of discrete categories
  *
- * Renders a <ul> with given options represented as <li> objects containing radio buttons
- *
- * Requires an 'options' value on the schema.
- *  Can be an array of options, a function that calls back with the array of options, a string of HTML
- *  or a Backbone collection. If a collection, the models must implement a toString() method
+ * see also BootstrapMenuMaker.js
  */
 
-require(['widgets/form/vendor/editors/BootstrapMenuMaker'],function(BootstrapMenuMaker){
+require([ 'widgets/form/vendor/editors/BootstrapMenuMaker' ], function(
+        BootstrapMenuMaker) {
     Backbone.Form.editors.Button = Backbone.Form.editors.Base.extend({
 
-          tagName: 'div',
-          //className: 'spotlight-toolbar',
+        tagName : 'div',
+        //className: 'spotlight-toolbar',
 
-          previousValue: '',
+        previousValue : '',
 
-          events: {
-            'keyup':    'determineChange',
-            'keypress': function(event) {
-              var self = this;
-              setTimeout(function() {
-                self.determineChange();
-              }, 0);
+        events : {
+            'keyup' : 'determineChange',
+            'keypress' : function(event) {
+                var self = this;
+                setTimeout(function() {
+                    self.determineChange();
+                }, 0);
             },
-            'change': function(event) {
-              this.trigger('change', this);
+            'change' : function(event) {
+                this.trigger('change', this);
             },
-            'focus':  function(event) {
-              this.trigger('focus', this);
+            'focus' : function(event) {
+                this.trigger('focus', this);
             },
-            'blur':   function(event) {
-              this.trigger('blur', this);
+            'blur' : function(event) {
+                this.trigger('blur', this);
             }
-          },
+        },
 
-          determineChange: function(event) {
+        determineChange : function(event) {
             var currentValue = this.getValue();
             var changed = (currentValue !== this.previousValue);
 
             if (changed) {
-              this.previousValue = currentValue;
+                this.previousValue = currentValue;
 
-              this.trigger('change', this);
+                this.trigger('change', this);
             }
-          },
+        },
 
-          /**
-           * Returns the template. Override for custom templates
-           *
-           * @return {Function}       Compiled template
-           */
-          getTemplate: function() {
-            return this.schema.template || this.constructor.template;
-          },
-
-          getValue: function() {
+        getValue : function() {
             return this.value;
-              //return this.$('input[type=radio]:checked').val();
-          },
+            //return this.$('input[type=radio]:checked').val();
+        },
 
-          setValue: function(value) {
+        setValue : function(value) {
             this.value = value;
-//		    this.$('input[type=radio]').val([value]);
-          },
+            //		    this.$('input[type=radio]').val([value]);
+        },
 
-          focus: function() {
-//		    if (this.hasFocus) return;
-        //
-//		    var checked = this.$('input[type=radio]:checked');
-//		    if (checked[0]) {
-//		      checked.focus();
-//		      return;
-//		    }
-        //
-//		    this.$('input[type=radio]').first().focus();
-          },
+        focus : function() {
+            //		    if (this.hasFocus) return;
+            //
+            //		    var checked = this.$('input[type=radio]:checked');
+            //		    if (checked[0]) {
+            //		      checked.focus();
+            //		      return;
+            //		    }
+            //
+            //		    this.$('input[type=radio]').first().focus();
+        },
 
-          blur: function() {
-//		    if (!this.hasFocus) return;
-        //
-//		    this.$('input[type=radio]:focus').blur();
-          },
+        blur : function() {
+            //		    if (!this.hasFocus) return;
+            //
+            //		    this.$('input[type=radio]:focus').blur();
+        },
 
-          render: function() {
-                this.$el.html(BootstrapMenuMaker.generateToolbar({"buttonGroupOne": this.schema.options}));
+        render : function() {
+            this.$el.html(BootstrapMenuMaker.generateToolbar({
+                "buttonGroupOne" : this.schema.options
+            }));
+            return this;
+        },
 
-
-                return this;
-              },
-
-
-
-        });
+    });
 
     return Backbone.Form.editors.Button;
 });

238e90d35bdcb22afdc75f32de45b7a6aaf5b83a
borismarin <borismarin@gmail.com>
2016-07-29 15:20:37 +0100

    added infomodal component

--- src/main/webapp/js/components/ComponentFactory.js
+++ src/main/webapp/js/components/ComponentFactory.js
@@ -31,82 +31,86 @@
  *******************************************************************************/
 
 define(function (require) {
-	
-	return function (GEPPETTO) {
 
-		var React = require('react');
-		var ReactDOM = require('react-dom');
-		var formComp = require('jsx!components/dev/form/Form');
-		var panelComp = require('jsx!components/dev/panel/Panel');
-		var logoComp = require('jsx!components/dev/logo/Logo');
-		var loadingSpinner = require('jsx!./loadingspinner/LoadingSpinner');
-		
-		GEPPETTO.ComponentFactory = {
-			getComponent: function(component, properties){
-				
-				if (component == 'FORM'){
-	    	      	return React.createFactory(formComp)(properties);
-				}
-				else if (component == 'PANEL'){
-					return React.createFactory(panelComp)(properties);
-				}
-				else if (component == 'LOGO'){
-					return React.createFactory(logoComp)(properties);
-				}
-				else if (component == 'LOADINGSPINNER'){
-					return React.createFactory(loadingSpinner)(properties);
-				}
-			},
-			
-			addComponent: function(component, properties, container){
-				return this.renderComponent(this.getComponent(component, properties), container);
-			},
-			
-			renderComponent: function(component, container){
-				//Let's create a dialog
-				if (container == undefined){
-					var containerId = component.props.id + "_container";
-					var containerName = component.props.name;
-					
-					//create the dialog window for the widget
-	                var dialog = $("<div id=" + containerId + " class='dialog' title='" + containerName + "'></div>").dialog(
-	                    {
-	                        resizable: true,
-	                        draggable: true,
-	                        top: 10,
-	                        height: 300,
-	                        width: 350,
-	                        close: function (event, ui) {
-	                            if (event.originalEvent &&
-	                                $(event.originalEvent.target).closest(".ui-dialog-titlebar-close").length) {
-	                                $("#" + this.id).remove();
-	                            }
-	                        }
-	                    });
-	
-	                var dialogParent = dialog.parent();
-	                var that = this;
-	
-	                //add history
-	                dialogParent.find("div.ui-dialog-titlebar").prepend("<div class='fa fa-history historyIcon'></div>");
-	                dialogParent.find("div.historyIcon").click(function (event) {
-	                    that.showHistoryMenu(event);
-	                    event.stopPropagation();
-	                });
-	
-	                //remove the jQuery UI icon
-	                dialogParent.find("button.ui-dialog-titlebar-close").html("");
-	                dialogParent.find("button").append("<i class='fa fa-close'></i>");
-	
-	
-	                //Take focus away from close button
-	                dialogParent.find("button.ui-dialog-titlebar-close").blur();	
-	                
-	                container = dialog.get(0);
-				}
-				
-				return ReactDOM.render(component, container);
-			}
-	    };
-	};
+    return function (GEPPETTO) {
+
+        var React = require('react');
+        var ReactDOM = require('react-dom');
+        var formComp = require('jsx!components/dev/form/Form');
+        var panelComp = require('jsx!components/dev/panel/Panel');
+        var logoComp = require('jsx!components/dev/logo/Logo');
+        var infoModalComp = require('jsx!components/popups/InfoModal');
+        var loadingSpinner = require('jsx!./loadingspinner/LoadingSpinner');
+
+        GEPPETTO.ComponentFactory = {
+            getComponent: function(component, properties){
+
+                if (component == 'FORM'){
+                      return React.createFactory(formComp)(properties);
+                }
+                else if (component == 'PANEL'){
+                    return React.createFactory(panelComp)(properties);
+                }
+                else if (component == 'LOGO'){
+                    return React.createFactory(logoComp)(properties);
+                }
+                else if (component == 'LOADINGSPINNER'){
+                    return React.createFactory(loadingSpinner)(properties);
+                }
+                else if (component == 'INFOMODAL'){
+                    return React.createFactory(infoModalComp)(properties);
+                }
+            },
+
+            addComponent: function(component, properties, container){
+                return this.renderComponent(this.getComponent(component, properties), container);
+            },
+
+            renderComponent: function(component, container){
+                //Let's create a dialog
+                if (container == undefined){
+                    var containerId = component.props.id + "_container";
+                    var containerName = component.props.name;
+
+                    //create the dialog window for the widget
+                    var dialog = $("<div id=" + containerId + " class='dialog' title='" + containerName + "'></div>").dialog(
+                        {
+                            resizable: true,
+                            draggable: true,
+                            top: 10,
+                            height: 300,
+                            width: 350,
+                            close: function (event, ui) {
+                                if (event.originalEvent &&
+                                    $(event.originalEvent.target).closest(".ui-dialog-titlebar-close").length) {
+                                    $("#" + this.id).remove();
+                                }
+                            }
+                        });
+
+                    var dialogParent = dialog.parent();
+                    var that = this;
+
+                    //add history
+                    dialogParent.find("div.ui-dialog-titlebar").prepend("<div class='fa fa-history historyIcon'></div>");
+                    dialogParent.find("div.historyIcon").click(function (event) {
+                        that.showHistoryMenu(event);
+                        event.stopPropagation();
+                    });
+
+                    //remove the jQuery UI icon
+                    dialogParent.find("button.ui-dialog-titlebar-close").html("");
+                    dialogParent.find("button").append("<i class='fa fa-close'></i>");
+
+
+                    //Take focus away from close button
+                    dialogParent.find("button.ui-dialog-titlebar-close").blur();
+
+                    container = dialog.get(0);
+                }
+
+                return ReactDOM.render(component, container);
+            }
+        };
+    };
 });

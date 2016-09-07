11ceab10811f4d15e4d8b87eb48c6c105008671d
johnidol <giovanni.idili@gmail.com>
2016-09-07 16:03:49 +0100

    formatting, remove unneeded dependency

--- src/main/webapp/js/components/dev/DropDownPanel/DropDownButton.js
+++ src/main/webapp/js/components/dev/DropDownPanel/DropDownButton.js
@@ -1,76 +1,76 @@
-define(function(require) {
+define(function (require) {
 
-	var React = require('react');
-	var ReactDOM = require('react-dom');
-	var GEPPETTO = require('geppetto');
+    var React = require('react');
+    var GEPPETTO = require('geppetto');
 
-	var comp = React.createClass({
-		getInitialState: function() {
-			return {
-				icon : this.props.iconOff,
-				label : this.props.label,
-				iconOn : this.props.iconOn,
-				iconOff : this.props.iconOff,
-				open : false,
-				disabled : true
-			};
-		},
+    var comp = React.createClass({
+        getInitialState: function () {
+            return {
+                icon: this.props.iconOff,
+                label: this.props.label,
+                iconOn: this.props.iconOn,
+                iconOff: this.props.iconOff,
+                open: false,
+                disabled: true
+            };
+        },
 
-		componentDidMount: function(){
-			var self = this;
-			GEPPETTO.on(Events.Experiment_active, function () {
-				if(!self.state.disabled){
-					if(GEPPETTO.DropDownPanel.isOpen()){
-						GEPPETTO.DropDownPanel.close();
-					}
-				}
-				var experiment = window.Project.getActiveExperiment();
-				var newState = true;
-				if(experiment!=null || undefined){
-					if(experiment.getStatus()==GEPPETTO.Resources.ExperimentStatus.COMPLETED){
-						newState = false;
-					}
-				}
-				self.setState({disabled: newState});
-			});
-			
-			GEPPETTO.on(Events.Experiment_completed, function (experimentID) {
-				var newState = self.state.disabled;
-				var experiment = window.Project.getActiveExperiment();
-				if(experiment.getId() == experimentID){
-					newState = false;
-				}
-				self.setState({disabled: newState});
-			});
-		},
+        componentDidMount: function () {
+            var self = this;
+            GEPPETTO.on(Events.Experiment_active, function () {
+                if (!self.state.disabled) {
+                    if (GEPPETTO.DropDownPanel.isOpen()) {
+                        GEPPETTO.DropDownPanel.close();
+                    }
+                }
+                var experiment = window.Project.getActiveExperiment();
+                var newState = true;
+                if (experiment != null || undefined) {
+                    if (experiment.getStatus() == GEPPETTO.Resources.ExperimentStatus.COMPLETED) {
+                        newState = false;
+                    }
+                }
+                self.setState({disabled: newState});
+            });
 
-		clickEvent : function(){
-			var showIcon;			if(GEPPETTO.DropDownPanel!=undefined){
-				if(GEPPETTO.DropDownPanel.isOpen()){
-					GEPPETTO.DropDownPanel.close();
-					showIcon = this.state.iconOff;
-					this.setState({open : false, icon : showIcon});
-				}else{
-					GEPPETTO.DropDownPanel.open();
-					showIcon = this.state.iconOn;
-					this.setState({open : true, icon : showIcon});
-				}
-			}  	
-		}, 
+            GEPPETTO.on(Events.Experiment_completed, function (experimentID) {
+                var newState = self.state.disabled;
+                var experiment = window.Project.getActiveExperiment();
+                if (experiment.getId() == experimentID) {
+                    newState = false;
+                }
+                self.setState({disabled: newState});
+            });
+        },
 
-		render:  function () {
-			return(
-					<div className="dropDownButtonContainer">
-    				<button className="btn dropDownButton pull-right" type="button" title=''
-    				onClick={this.clickEvent} disabled={this.state.disabled}>
-    					<i className={this.state.icon}></i>
-    					{this.state.label}
-    				</button>
-    			</div>
-			);
-		}
-	});
-    
+        clickEvent: function () {
+            var showIcon;
+            if (GEPPETTO.DropDownPanel != undefined) {
+                if (GEPPETTO.DropDownPanel.isOpen()) {
+                    GEPPETTO.DropDownPanel.close();
+                    showIcon = this.state.iconOff;
+                    this.setState({open: false, icon: showIcon});
+                } else {
+                    GEPPETTO.DropDownPanel.open();
+                    showIcon = this.state.iconOn;
+                    this.setState({open: true, icon: showIcon});
+                }
+            }
+        },
+
+        render: function () {
+            return (
+                <div className="dropDownButtonContainer">
+                    <button className="btn dropDownButton pull-right" type="button" title=''
+                            onClick={this.clickEvent} disabled={this.state.disabled}>
+                        <i className={this.state.icon}></i>
+                        {this.state.label}
+                    </button>
+                </div>
+            );
+        }
+    });
+
     return comp;
 
 });

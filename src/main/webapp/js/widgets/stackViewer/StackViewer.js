977ef6d9e2d6010e77d2e8e872bd98cf03817789
Robert Court <robbie.court@gmail.com>
2016-10-13 15:43:11 +0100

    specifying jsx

--- src/main/webapp/js/widgets/stackViewer/StackViewer.js
+++ src/main/webapp/js/widgets/stackViewer/StackViewer.js
@@ -39,8 +39,8 @@ define(function (require) {
 
     var Widget = require('widgets/Widget');
     var $ = require('jquery');
-    var React = require('widgets/stackViewer/vendor/react.min');
-    var ReactDOM = require('widgets/stackViewer/vendor/react-dom.min');
+    var React = require('jsx!widgets/stackViewer/vendor/react.min');
+    var ReactDOM = require('jsx!widgets/stackViewer/vendor/react-dom.min');
     var StackViewerComponent = require('jsx!widgets/stackViewer/StackViewerComponent');
 
     return Widget.View.extend({

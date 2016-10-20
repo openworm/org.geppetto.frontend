5e2a884a401b2c0234edf3cf2ace601a437de7c9
Robert Court <robbie.court@gmail.com>
2016-10-20 12:37:39 +0100

    syntax fix

--- src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
+++ src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
@@ -745,7 +745,7 @@ define(function (require) {
                 for (instance in instances) {
                     try {
                         vals = instance.getVariable().getInitialValue().value;
-                        console.log(JSON.parse(vals));
+                        console.log(vals);
                         data = JSON.parse(vals.data);
                         server = data.serverUrl;
                         files.push(data.fileLocation);

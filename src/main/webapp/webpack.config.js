48f8cc506d4a7a8ce22ba8ed0c6d6e7d16507c22
Adrian Quintana <adrianquintana@gmail.com>
2016-10-26 00:05:05 +0100

    indentantion, remove comments and unneeded configurations

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 
 const PATHS = {
   //build: path.join(__dirname, 'target', 'classes', 'META-INF', 'resources', 'webjars', packageJSON.name, packageJSON.version)
-		build: path.join(__dirname, 'target', 'classes', 'static')
+  build: path.join(__dirname, 'target', 'classes', 'static')
 };
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
   devtool: 'source-map',
   output: {
     //path: PATHS.build,
-	 path: './js/',
+    path: './js/',
     filename: 'app-bundle.js',
     publicPath: "/org.geppetto.frontend/geppetto/js/",
   },
@@ -20,39 +20,39 @@ module.exports = {
     extensions: ['', '.js', '.json']
   },
   // target: "node",
-  
-  module: {
-      loaders: [
-          { test: /\.css$/, loader: "style!css" },
-        {
-            test: /\.js?/,
-            exclude: /node_modules/,
-            loader: ['babel-loader'],
-            query: {
-              presets: ['react', 'es2015']
-            }
-        },
-        { test: /\.json$/, loader: "json-loader" },
-        {
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-          ]
-        }
 
-      ]
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: "style!css" },
+      {
+        test: /\.js?/,
+        exclude: /node_modules/,
+        loader: ['babel-loader'],
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      { test: /\.json$/, loader: "json-loader" },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+
+    ]
   },
 
-//  resolve : {
-//	    alias: {
-//	      // bind version of jquery-ui
-//	      "jquery-ui": "jquery-ui/jquery-ui.js",      
-//	      // bind to modules;
-//	      modules: path.join(__dirname, "node_modules"),
-//	    }
-//	},
-  
+  //  resolve : {
+  //	    alias: {
+  //	      // bind version of jquery-ui
+  //	      "jquery-ui": "jquery-ui/jquery-ui.js",      
+  //	      // bind to modules;
+  //	      modules: path.join(__dirname, "node_modules"),
+  //	    }
+  //	},
+
   node: {
     fs: 'empty',
     child_process: 'empty',

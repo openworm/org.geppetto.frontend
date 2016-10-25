38d005067397a618c7083a5b4b698016bb5cbf86
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2016-10-25 12:20:21 +0100

    Fix for spinner stop spinning when switching project. Fix for experiment controls wrong state on play.

--- src/main/webapp/js/components/dev/logo/Logo.js
+++ src/main/webapp/js/components/dev/logo/Logo.js
@@ -43,11 +43,12 @@ define(function (require) {
     var logoDiv = React.createClass({
         componentDidMount: function () {
             GEPPETTO.on('spin_logo', function (label) {
-                this.addClass("fa-spin").attr('title', 'Loading data');
+            	//TODO Fix this to use state instead and not touching the dom element with jQuery
+                $("#geppettologo").addClass("fa-spin").attr('title', 'Loading data');
             }.bind($("." + this.props.logo)));
 
             GEPPETTO.on('stop_spin_logo', function (label) {
-                this.removeClass("fa-spin").attr('title', '');
+            	$("#geppettologo").removeClass("fa-spin").attr('title', '');
             }.bind($("." + this.props.logo)));
         },
 

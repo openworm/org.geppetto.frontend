e0a80c93483142e727028de6b3995aa982d5d1b5
Robert Court <robbie.court@gmail.com>
2016-10-29 23:00:17 +0100

    adding dst

--- src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
+++ src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
@@ -188,7 +188,7 @@ define(function (require) {
         callPlaneEdges: function() {
             if (!this.state.planeUpdating) {
                 this.state.planeUpdating = true;
-                var image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[0] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + this.props.scl.toFixed(1) + '&dst=0&pit=' + Number(this.props.pit).toFixed(0) + '&yaw=' + Number(this.props.yaw).toFixed(0) + '&rol=' + Number(this.props.rol).toFixed(0);
+                var image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[0] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + this.props.scl.toFixed(1) + '&dst=' + Number(this.props.dst).toFixed(1) + '&pit=' + Number(this.props.pit).toFixed(0) + '&yaw=' + Number(this.props.yaw).toFixed(0) + '&rol=' + Number(this.props.rol).toFixed(0);
                 //get top left plane coordinate:
                 var coordinates = [];
                 var x, y;

8d73f8202844cab9c533f7b8e818171d3a76faa9
Robert Court <robbie.court@gmail.com>
2016-11-07 13:30:02 +0000

    zoom on disp

--- src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
+++ src/main/webapp/js/widgets/stackViewer/StackViewerComponent.js
@@ -206,8 +206,8 @@ define(function (require) {
                     var coordinates = [];
                     var x, y, z;
                     // update widget window extents (X,Y) :
-                    x = ((this.stack.width * 0.5) - (this.stage.position.x + this.stack.position.x))/this.stage.scale.x;
-                    y = ((this.stack.height * 0.5) - (this.stage.position.y + this.stack.position.y))/this.stage.scale.y;
+                    x = ((this.stack.width * 0.5) - (this.stage.position.x + this.stack.position.x))/this.disp.scale.x;
+                    y = ((this.stack.height * 0.5) - (this.stage.position.y + this.stack.position.y))/this.disp.scale.y;
                     coordinates[0] = x.toFixed(0);
                     coordinates[1] = y.toFixed(0);
                     x = x + this.renderer.view.width;

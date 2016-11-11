a8e1e2b05b225e2e01630fd9d56cd0a7c8501868
Adrian Quintana <adrianquintana@gmail.com>
2016-11-11 08:24:07 +0000

    added plot and widget base class sync

--- src/main/webapp/js/components/jupyter/GeppettoJupyter.js
+++ src/main/webapp/js/components/jupyter/GeppettoJupyter.js
@@ -360,6 +360,57 @@ define(function (require, exports, module) {
 			}, jupyter_widgets.WidgetModel.serializers)
 		});
 
+
+	var WidgetSync = jupyter_widgets.WidgetModel.extend({
+		defaults: _.extend({}, jupyter_widgets.WidgetModel.prototype.defaults, {
+			widget_id: '',
+			name: '',
+			data: [],
+			positionX: null,
+			positionY: null,
+			widget_object: null
+		}),
+
+		initialize: function () {
+			WidgetSync.__super__.initialize.apply(this);
+
+
+
+			if (this.get('widget_id') > -1) {
+				var widget = G.addWidget(this.get('widget_id'))
+				this.set('widget_object', widget)
+
+				// if (this.get('data').length > 0) {
+				// 	for (var i = 0; i < this.get('data').length; i++){
+				// 		widget.setData(eval(this.get('data')[i]))
+				// 	}
+				// }
+				if (this.get('name') != '') {
+					widget.setName(this.get('name'))
+				}
+				if (this.get('positionX') > 0 && this.get('positionY') > 0) {
+				}
+
+			}
+		}
+	});
+
+	var PlotWidgetSync = WidgetSync.extend({
+		_model_name: 'PlotWidgetSync',
+		_model_module: "model",
+
+		initialize: function () {
+			PlotWidgetSync.__super__.initialize.apply(this);
+
+			if (this.get('data').length > 0) {
+				for (var i = 0; i < this.get('data').length; i++){
+					this.get('widget_object').plotData(eval(this.get('data')[i]))
+				}
+			}
+		}
+	});
+
+
 	module.exports = {
 		PanelView: PanelView,
 		PanelModel: PanelModel,
@@ -368,6 +419,8 @@ define(function (require, exports, module) {
 		StateVariableSync: StateVariableSync,
 		ModelSync: ModelSync,
 		ExperimentSync: ExperimentSync,
-		ProjectSync: ProjectSync
+		ProjectSync: ProjectSync,
+		WidgetSync: WidgetSync,
+		PlotWidgetSync: PlotWidgetSync
 	};
 });

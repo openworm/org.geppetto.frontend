47efc3ac639e6ba1432b5ca1072f63c6f2ce676e
Adrian Quintana <adrianquintana@gmail.com>
2016-11-23 13:26:51 +0000

    fix extension load for npm. Extract loadextension to a function

--- src/main/webapp/extensions/extensions.js
+++ src/main/webapp/extensions/extensions.js
@@ -1,11 +1,16 @@
 define(['./extensionsConfiguration.json', 'geppetto','../js/components/ComponentFactory'],function(extensionConfiguration, GEPPETTO) {
+	//Load extension depending on extension path and init componentsInitialization.
+	//Note this needs to be an independent function due to require async nature 
+	function loadExtension(extensionPath){
+		require(['../extensions/' + extensionPath], function(componentsInitialization){
+			componentsInitialization(GEPPETTO);
+		});
+	}
 
 	//Require your extension in extensionConfiguration.json
 	for (var extension in extensionConfiguration){
 		if (extensionConfiguration[extension]){
-			require(['../extensions/' + extension], function(componentsInitialization){
-				componentsInitialization(GEPPETTO);
-			});
+			loadExtension(extension);
 		}
 	}
 });

689d573c2b5b791d78d4e97e51d6d16d23afd0db
Adrian Quintana <adrianquintana@gmail.com>
2016-12-21 12:05:50 +0000

    some work with splitmeshes

--- src/main/webapp/js/GEPPETTO.js
+++ src/main/webapp/js/GEPPETTO.js
@@ -416,6 +416,16 @@ define(function (require) {
                 }
             });
 
+            for (var keySplitMeshes in GEPPETTO.getVARS().splitMeshes) {
+                var child = GEPPETTO.getVARS().splitMeshes[keySplitMeshes]
+                if (child.visible && !(child.clickThrough==true)) {
+                    if (child.geometry != null || undefined) {
+                        child.geometry.computeBoundingBox();
+                    }
+                    visibleChildren.push(child);
+                }
+            }
+
             // returns an array containing all objects in the scene with which
             // the ray intersects
             return raycaster.intersectObjects(visibleChildren);

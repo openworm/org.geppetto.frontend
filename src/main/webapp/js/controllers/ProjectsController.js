b230e8c916642a70f64e798342a281b32db87fff
johnidol <giovanni.idili@gmail.com>
2017-01-16 15:18:11 +0000

    add API methods to ProjectsController

--- src/main/webapp/js/controllers/ProjectsController.js
+++ src/main/webapp/js/controllers/ProjectsController.js
@@ -33,10 +33,10 @@
 /**
  * Controller responsible to manage the projects
  * @author Matteo Cantarelli
+ * @author Giovanni Idili
  */
 define(function (require) {
     return function (GEPPETTO) {
-
         /**
          * @class GEPPETTO.ProjectsController
          */
@@ -46,9 +46,82 @@ define(function (require) {
 
             getUserProjects: function(){
             	return this.userProjects;
+            },
+
+            getProjectStateVariables: function(projectId){
+                return this.getProjectData(projectId, GEPPETTO.Resources.STATE_VARIABLE_TYPE);
+            },
+
+            getGlobalStateVariables: function(projectId, includeProject){
+                return this.getProjectData(projectId, GEPPETTO.Resources.STATE_VARIABLE_TYPE, true, includeProject);
+            },
+
+            getProjectParameters: function(projectId){
+                return this.getProjectData(projectId, GEPPETTO.Resources.PARAMETER_TYPE);
+            },
+
+            getGlobalParameters: function(projectId, includeProject){
+                return this.getProjectData(projectId, GEPPETTO.Resources.PARAMETER_TYPE, true, includeProject);
+            },
+
+            getProjectData: function (projectId, dataType, globalScope, includeProject) {
+                if(globalScope == undefined){
+                    globalScope = false;
+                }
+                if(includeProject == undefined){
+                    includeProject = true;
+                }
+
+                var data = [];
+
+                var projects = this.getUserProjects();
+
+                // get all experiments from current project
+                for (var i = 0; i < projects.length; i++) {
+                    // include projectId only if include flag is true / inlcude other projects only if global flag is true
+                    if ((projects[i].id == projectId) && includeProject) {
+                        data = data.concat(this.getData(projects[i], dataType));
+                    } else if ((projects[i].id != projectId) && globalScope){
+                        data = data.concat(this.getData(projects[i], dataType));
+                    }
+                }
+
+                return data;
+            },
+
+            getData: function (project, dataType) {
+                var data = [];
+
+                // get all state variables for completed experiments
+                var experiments = project.experiments;
+                for (var j = 0; j < experiments.length; j++) {
+                    if (experiments[j].status == GEPPETTO.Resources.ExperimentStatus.COMPLETED) {
+                        var dataSource = (dataType == GEPPETTO.Resources.STATE_VARIABLE_TYPE) ?
+                            experiments[j].aspectConfigurations[0].watchedVariables :
+                            experiments[j].aspectConfigurations[0].modelParameter;
+                        data = data.concat(dataSource.map(
+                            function (item) {
+                                return {
+                                    path: item,
+                                    name: item,
+                                    type: ['Model.common.' + (dataType == GEPPETTO.Resources.STATE_VARIABLE_TYPE) ? 'StateVariable' : 'Parameter'],
+                                    projectId: project.id,
+                                    projectName: project.name,
+                                    experimentId: experiments[j].id,
+                                    experimentName: experiments[j].name,
+                                    getPath: function () {
+                                        return this.path;
+                                    }
+                                }
+                            })
+                        );
+                    }
+                }
+
+                return data;
             }
         };
-        
+
         //We execute an asynchronous call to fetch all the projects for the current user
         $.ajax({
 	        url: window.location.origin + window.location.pathname + "projects"

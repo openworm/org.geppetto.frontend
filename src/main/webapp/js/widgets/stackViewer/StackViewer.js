11f4f12237963a2505969b07d7d48271d0e9d863
Rob Court <robbie.court@gmail.com>
2017-01-26 15:19:03 +0000

    typo fix

--- src/main/webapp/js/widgets/stackViewer/StackViewer.js
+++ src/main/webapp/js/widgets/stackViewer/StackViewer.js
@@ -222,12 +222,12 @@ define(function (require) {
 	getHelp: function(){
        	    return '### Stack Viewer Help\n\n' +
             'The stack viewer allows you to explore slices through the current stack. \n\n' +
-	    'To move around the slice either click and drag or use a 3 fingure drag gesture (when over the image). \n\n' +
+	    'To move around the slice either click and drag or use a 3 finger drag gesture (when over the image). \n\n' +
 	    'Hovering shows items that can be added by clicking in combination with the shift key. \n\n' +
 	    'Clicking on any item displayed in the stack selects it. \n\n' +
             'Zoom level is controlled by either 2 finger pinch, ctrl + mouse wheel or the <i class="fa fa-search-plus" aria-hidden="true"></i> / <i class="fa fa-search-minus" aria-hidden="true"></i> icons. \n\n' +
 	    'Stack position is controlled via 2 finger scroll, mouse wheel or the <i class="fa fa-chevron-up" aria-hidden="true"></i> / <i class="fa fa-chevron-down" aria-hidden="true"></i> icons. \n\n' + 
-            'The orthoginal orientation of the slice is toggled by the <i class="fa fa-refresh" aria-hidden="true"></i> icon.';
+            'The orthogonal orientation of the slice is toggled by the <i class="fa fa-refresh" aria-hidden="true"></i> icon.';
         },
 
         destroy: function () {

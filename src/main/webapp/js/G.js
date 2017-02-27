1d1839f50cc0849fecb721ff3c6bcfbbeffc12e0
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-02-27 16:59:43 +0000

    Path update

--- src/main/webapp/js/G.js
+++ src/main/webapp/js/G.js
@@ -44,7 +44,7 @@ define(function (require) {
         var debugMode = false;
         var $ = require('jquery'),
             React = require('react'),
-            ClipboardModal = require('../components/modals/ClipboardModal');
+            ClipboardModal = require('./components/modals/ClipboardModal');
 
         /**
          * @exports geppetto-objects/G

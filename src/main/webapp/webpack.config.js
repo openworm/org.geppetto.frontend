295b45e04073f3b2b64e8cfdc24f43acf9e3d841
johnidol <giovanni.idili@gmail.com>
2017-02-28 14:17:21 +0000

    exclude bundle files from loaders

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'],
+      { test: /\.js$/, exclude: [/node_modules/, /\w*\.bundle\.js$/], loader: ['babel-loader'],
         query: {
           presets: ['react', 'es2015']
         }

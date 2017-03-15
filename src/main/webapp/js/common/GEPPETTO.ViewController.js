eb595517e36ff976720f58204362aad7171ef2a6
johnidol <giovanni.idili@gmail.com>
2017-03-15 18:23:11 +0000

    more skeleton TODO pseudo-code

--- src/main/webapp/js/common/GEPPETTO.ViewController.js
+++ src/main/webapp/js/common/GEPPETTO.ViewController.js
@@ -6,7 +6,28 @@ define(function(require)
 {
     return function(GEPPETTO) {
         GEPPETTO.ViewController = {
+            /**
+             * Applies initial view state for project / experiment and sets up monitor
+             */
+            applyView: function(projectView, experimentView){
+                // TODO: apply project view
+                // TODO: apply experirment view
+                // TODO: foreach view item
+                    // TODO: create widget if it doesn't exist
+                    // TODO: apply properties using setView API
+                // TODO: setup monitor loop to track changes every 1000ms
+            },
 
+            /**
+             * Monitors changes in the view
+             */
+            monitorView: function(){
+                // TODO: retrieve list of widgets (components in future)
+                // TODO: foreach widget/component in the list
+                    // TODO: call getView API
+                // TODO: build view json with view state for all the widgets/components
+                // TODO: call a new setExperimentView method on the *new* web socket api
+            }
         };
     };
 });

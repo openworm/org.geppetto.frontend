7c345958a81a801fff741342c82166f98b19068a
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-03-21 17:23:13 +0000

    Mesh var removed

--- src/main/webapp/js/components/interface/3dCanvas/OBJLoader.js
+++ src/main/webapp/js/components/interface/3dCanvas/OBJLoader.js
@@ -446,12 +446,7 @@ THREE.OBJLoader.prototype = {
             });
         material.defaultColor = color;
         material.defaultOpacity = 1;
-        var mesh = new THREE.Points(geometry, material);
-
-
-        return mesh;
-
-
+        return new THREE.Points(geometry, material);
     },
 
     parse: function (text) {

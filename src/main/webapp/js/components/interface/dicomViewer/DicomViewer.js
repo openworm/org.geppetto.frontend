a97cec047ff07fa816ea6a7cc4ff8d4617d1f8e9
Adrian Quintana <adrianquintana@gmail.com>
2017-03-26 13:39:32 +0100

    simplified version and remove tests bits

--- src/main/webapp/js/components/interface/dicomViewer/DicomViewer.js
+++ src/main/webapp/js/components/interface/dicomViewer/DicomViewer.js
@@ -17,454 +17,190 @@ define(function (require) {
 		},
 
 		componentDidMount: function () {
-			/* globals Stats, dat, AMI*/
 
+			// VJS classes we will be using in this lesson
 			var LoadersVolume = AMI.default.Loaders.Volume;
 			var CamerasOrthographic = AMI.default.Cameras.Orthographic;
 			var ControlsOrthographic = AMI.default.Controls.TrackballOrtho;
-			var HelpersLut = AMI.default.Helpers.Lut;
 			var HelpersStack = AMI.default.Helpers.Stack;
 
-			// Shaders
-			// Data
-			var ShadersDataUniforms = AMI.default.Shaders.DataUniform;
-			var ShadersDataFragment = AMI.default.Shaders.DataFragment;
-			var ShadersDataVertex = AMI.default.Shaders.DataVertex;
-			// Layer
-			var ShadersLayerUniforms = AMI.default.Shaders.LayerUniform;
-			var ShadersLayerFragment = AMI.default.Shaders.LayerFragment;
-			var ShadersLayerVertex = AMI.default.Shaders.LayerVertex;
+			// Setup renderer
+			var container = document.getElementById('dicomViewer');
+			container.style.height = "300px";
+			container.style.width = "350px";
+			var renderer = new THREE.WebGLRenderer({
+				antialias: true,
+			});
+			renderer.setSize(container.offsetWidth, container.offsetHeight);
+			renderer.setClearColor(0x353535, 1);
+			renderer.setPixelRatio(window.devicePixelRatio);
+			container.appendChild(renderer.domElement);
 
-			// standard global variables
-			var controls;
-			var renderer;
-			var camera;
-			//var statsyay;
-			var threeD;
-			//
-			var sceneLayer0TextureTarget;
-			var sceneLayer1TextureTarget;
-			//
-			var sceneLayer0;
-			//
-			var lutLayer0;
-			var sceneLayer1;
-			var meshLayer1;
-			var uniformsLayer1;
-			var materialLayer1;
-			var lutLayer1;
-			var sceneLayerMix;
-			var meshLayerMix;
-			var uniformsLayerMix;
-			var materialLayerMix;
+			// Setup scene
+			var scene = new THREE.Scene();
 
-			var layerMix = {
-				opacity1: 1.0,
-			};
+			// Setup camera
+			var camera = new CamerasOrthographic(
+				container.clientWidth / -2, container.clientWidth / 2,
+				container.clientHeight / 2, container.clientHeight / -2,
+				0.1, 10000);
+
+			// Setup controls
+			var controls = new ControlsOrthographic(camera, container);
+			controls.staticMoving = true;
+			controls.noRotate = true;
+			camera.controls = controls;
 
 			/**
-			 * Init the scene
+			 * Handle window resize
 			 */
-			function init() {
-				/**
-				 * Animation loop
-				 */
-				function animate() {
-					// render
-					controls.update();
-					// render first layer offscreen
-					renderer.render(sceneLayer0, camera, sceneLayer0TextureTarget, true);
-					// render second layer offscreen
-					renderer.render(sceneLayer1, camera, sceneLayer1TextureTarget, true);
-					// mix the layers and render it ON screen!
-					renderer.render(sceneLayerMix, camera);
-					//statsyay.update();
+			function onWindowResize() {
+				camera.canvas = {
+					width: container.offsetWidth,
+					height: container.offsetHeight,
+				};
+				camera.fitBox(2,2);
 
-					// request new frame
-					requestAnimationFrame(function () {
-						animate();
-					});
-				}
-
-				// renderer
-				threeD = document.getElementById('dicomViewer');
-				threeD.style.height = "300px";
-				threeD.style.width = "350px";
-				renderer = new THREE.WebGLRenderer({
-					antialias: true,
-					alpha: true,
-				});
-				renderer.setSize(threeD.clientWidth, threeD.clientHeight);
-				renderer.setClearColor(0x607D8B, 1);
-
-				threeD.appendChild(renderer.domElement);
-
-				// stats
-				// statsyay = new Stats();
-				// threeD.appendChild(statsyay.domElement);
-
-				// scene
-				sceneLayer0 = new THREE.Scene();
-				sceneLayer1 = new THREE.Scene();
-				sceneLayerMix = new THREE.Scene();
-
-				// render to texture!!!!
-				sceneLayer0TextureTarget = new THREE.WebGLRenderTarget(
-					threeD.clientWidth,
-					threeD.clientHeight,
-					{
-						minFilter: THREE.LinearFilter,
-						magFilter: THREE.NearestFilter,
-						format: THREE.RGBAFormat,
-					});
-
-				sceneLayer1TextureTarget = new THREE.WebGLRenderTarget(
-					threeD.clientWidth,
-					threeD.clientHeight,
-					{
-						minFilter: THREE.LinearFilter,
-						magFilter: THREE.NearestFilter,
-						format: THREE.RGBAFormat,
-					});
-
-				// camera
-				camera = new CamerasOrthographic(
-					threeD.clientWidth / -2, threeD.clientWidth / 2,
-					threeD.clientHeight / 2, threeD.clientHeight / -2,
-					0.1, 10000);
-
-				// controls
-				controls = new ControlsOrthographic(camera, threeD);
-				controls.staticMoving = true;
-				controls.noRotate = true;
-				camera.controls = controls;
-
-				animate();
+				renderer.setSize(container.offsetWidth, container.offsetHeight);
 			}
+			window.addEventListener('resize', onWindowResize, false);
 
-			// init threeJS...
-			init();
+			 $("#" + this.props.containerid ).on("dialogresizestop", function (event, ui) {
+				camera.canvas = {
+					width:  ui.size.width-30,
+					height: ui.size.height-30,
+				};
+				camera.fitBox(2,2);
 
-			var data = [
-				'000183.dcm', '000219.dcm', '000117.dcm',
-				'000240.dcm', '000033.dcm', '000060.dcm',
-				'000211.dcm', '000081.dcm', '000054.dcm',
-				'000090.dcm', '000042.dcm', '000029.dcm',
-				'000239.dcm', '000226.dcm', '000008.dcm',
-				'000128.dcm', '000089.dcm', '000254.dcm',
-				'000208.dcm', '000047.dcm', '000067.dcm',
-			];
-
-			var rawgit =
-				'https://cdn.rawgit.com/FNNDSC/data/master/dicom/andrei_abdomen/';
-
-			var dataFullPath = data.map(function (v) {
-				return rawgit + 'data/' + v;
+				renderer.setSize( ui.size.width-30, ui.size.height-30);
+				
 			});
 
-			var labelmap = [
-				'000000.dcm',
-			];
+			/**
+			 * Start animation loop
+			 */
+			function animate() {
+				controls.update();
+				renderer.render(scene, camera);
 
-			var labelmapFullPath = labelmap.map(function (v) {
-				return rawgit + 'segmentation/' + v;
-			});
+				// request new frame
+				requestAnimationFrame(function () {
+					animate();
+				});
+			}
+			animate();
 
-			var files = dataFullPath.concat(labelmapFullPath);
+			// Setup loader
+			// var loader = new LoadersVolume(container);
+			// var file =
+			// 	'https://cdn.rawgit.com/FNNDSC/data/master/nifti/adi_brain/adi_brain.nii.gz';
 
-			// load sequence for each file
 			// instantiate the loader
 			// it loads and parses the dicom image
-			var loader = new LoadersVolume(threeD);
+			var loader = new LoadersVolume(container);
 
-			/**
-			 * Build GUI
-			 */
-			function buildGUI(stackHelper) {
-				/**
-				 * Update Layer 1
-				 */
-				function updateLayer1() {
-					// update layer1 geometry...
-					if (meshLayer1) {
-						// dispose geometry first
-						meshLayer1.geometry.dispose();
-						meshLayer1.geometry = stackHelper.slice.geometry;
-						meshLayer1.geometry.verticesNeedUpdate = true;
-					}
-				}
+			var t2 = [
+				'00010001.dcm', '00010019.dcm', '00010037.dcm', '00010055.dcm', '00010073.dcm', '00010091.dcm', '00010109.dcm', '00010127.dcm',
+				'00010002.dcm', '00010020.dcm', '00010038.dcm', '00010056.dcm', '00010074.dcm', '00010092.dcm', '00010110.dcm', '00010128.dcm',
+				'00010003.dcm', '00010021.dcm', '00010039.dcm', '00010057.dcm', '00010075.dcm', '00010093.dcm', '00010111.dcm', '00010129.dcm',
+				'00010004.dcm', '00010022.dcm', '00010040.dcm', '00010058.dcm', '00010076.dcm', '00010094.dcm', '00010112.dcm', '00010130.dcm',
+				'00010005.dcm', '00010023.dcm', '00010041.dcm', '00010059.dcm', '00010077.dcm', '00010095.dcm', '00010113.dcm', '00010131.dcm',
+				'00010006.dcm', '00010024.dcm', '00010042.dcm', '00010060.dcm', '00010078.dcm', '00010096.dcm', '00010114.dcm', '00010132.dcm',
+				'00010007.dcm', '00010025.dcm', '00010043.dcm', '00010061.dcm', '00010079.dcm', '00010097.dcm', '00010115.dcm', '00010133.dcm',
+				'00010008.dcm', '00010026.dcm', '00010044.dcm', '00010062.dcm', '00010080.dcm', '00010098.dcm', '00010116.dcm', '00010134.dcm',
+				'00010009.dcm', '00010027.dcm', '00010045.dcm', '00010063.dcm', '00010081.dcm', '00010099.dcm', '00010117.dcm', '00010135.dcm',
+				'00010010.dcm', '00010028.dcm', '00010046.dcm', '00010064.dcm', '00010082.dcm', '00010100.dcm', '00010118.dcm', '00010136.dcm',
+				'00010011.dcm', '00010029.dcm', '00010047.dcm', '00010065.dcm', '00010083.dcm', '00010101.dcm', '00010119.dcm', '00010137.dcm',
+				'00010012.dcm', '00010030.dcm', '00010048.dcm', '00010066.dcm', '00010084.dcm', '00010102.dcm', '00010120.dcm', '00010138.dcm',
+				'00010013.dcm', '00010031.dcm', '00010049.dcm', '00010067.dcm', '00010085.dcm', '00010103.dcm', '00010121.dcm',
+				'00010014.dcm', '00010032.dcm', '00010050.dcm', '00010068.dcm', '00010086.dcm', '00010104.dcm', '00010122.dcm',
+				'00010015.dcm', '00010033.dcm', '00010051.dcm', '00010069.dcm', '00010087.dcm', '00010105.dcm', '00010123.dcm',
+				'00010016.dcm', '00010034.dcm', '00010052.dcm', '00010070.dcm', '00010088.dcm', '00010106.dcm', '00010124.dcm',
+				'00010017.dcm', '00010035.dcm', '00010053.dcm', '00010071.dcm', '00010089.dcm', '00010107.dcm', '00010125.dcm',
+				'00010018.dcm', '00010036.dcm', '00010054.dcm', '00010072.dcm', '00010090.dcm', '00010108.dcm', '00010126.dcm',
+			];
 
-				/**
-				 * Update layer mix
-				 */
-				function updateLayerMix() {
-					// update layer1 geometry...
-					if (meshLayerMix) {
-						sceneLayerMix.remove(meshLayerMix);
-						meshLayerMix.material.dispose();
-						meshLayerMix.material = null;
-						meshLayerMix.geometry.dispose();
-						meshLayerMix.geometry = null;
-
-						// add mesh in this scene with right shaders...
-						meshLayerMix = new THREE.Mesh(
-							stackHelper.slice.geometry, materialLayerMix);
-						// go the LPS space
-						meshLayerMix.applyMatrix(stackHelper.stack._ijk2LPS);
-
-						sceneLayerMix.add(meshLayerMix);
-					}
-				}
-
-				var stack = stackHelper.stack;
-
-				// var gui = new dat.GUI({
-				// 	autoPlace: false,
-				// });
-
-				// var customContainer = document.getElementById('my-gui-container');
-				// customContainer.appendChild(gui.domElement);
-
-				// var layer0Folder = gui.addFolder('CT');
-				// layer0Folder.add(stackHelper.slice, 'invert');
-
-				// var lutUpdate = layer0Folder.add(
-				// 	stackHelper.slice, 'lut', lutLayer0.lutsAvailable());
-				// lutUpdate.onChange(function (value) {
-				// 	lutLayer0.lut = value;
-				// 	stackHelper.slice.lutTexture = lutLayer0.texture;
-				// });
-
-				// var indexUpdate = layer0Folder.add(
-				// 	stackHelper, 'index', 0, stack.dimensionsIJK.z - 1).step(1).listen();
-				// indexUpdate.onChange(function () {
-				// 	updateLayer1();
-				// 	updateLayerMix();
-				// });
-
-				// layer0Folder.add(
-				// 	stackHelper.slice, 'interpolation', 0, 1).step(1).listen();
-
-				// layer0Folder.open();
-
-				// // layer mix folder
-				// var layerMixFolder = gui.addFolder('Segmentation');
-				// var opacityLayerMix1 = layerMixFolder.add(
-				// 	layerMix, 'opacity1', 0, 1).step(0.01);
-				// opacityLayerMix1.onChange(function (value) {
-				// 	uniformsLayerMix.uOpacity1.value = value;
-				// });
-
-				// layerMixFolder.open();
-
-				// hook up callbacks
-				controls.addEventListener('OnScroll', function (e) {
-					if (e.delta > 0) {
-						if (stackHelper.index >= stack.dimensionsIJK.z - 1) {
-							return false;
-						}
-						stackHelper.index += 1;
-					} else {
-						if (stackHelper.index <= 0) {
-							return false;
-						}
-						stackHelper.index -= 1;
-					}
-
-					updateLayer1();
-					updateLayerMix();
-				});
-
-				updateLayer1();
-				updateLayerMix();
-
-				/**
-				 * Handle window resize
-				 */
-				function onWindowResize() {
-					var threeD = document.getElementById('dicomViewer');
-					camera.canvas = {
-						width: threeD.clientWidth,
-						height: threeD.clientHeight,
-					};
-					camera.fitBox(2);
-
-					renderer.setSize(threeD.clientWidth, threeD.clientHeight);
-				}
-				window.addEventListener('resize', onWindowResize, false);
-				onWindowResize();
-			}
-
-			/**
-			 * Handle series
-			 */
-			function handleSeries() {
-				//
-				//
-				// first stack of first series
-				var mergedSeries = loader.data[0].mergeSeries(loader.data);
-				var stack = mergedSeries[0].stack[0];
-				var stack2 = mergedSeries[1].stack[0];
-				loader.free();
-				loader = null;
-
-				if (stack.modality === 'SEG') {
-					stack = mergedSeries[0].stack[0];
-					stack2 = mergedSeries[1].stack[0];
-				}
-
-				var stackHelper = new HelpersStack(stack);
-				stackHelper.bbox.visible = false;
-				stackHelper.border.visible = false;
-				stackHelper.index = 10;
-
-				sceneLayer0.add(stackHelper);
-
-				//
-				//
-				// create labelmap....
-				// we only care about the geometry....
-				// get first stack from series
-				// prepare it
-				// * ijk2LPS transforms
-				// * Z spacing
-				// * etc.
-				//
-				stack2.prepare();
-				// pixels packing for the fragment shaders now happens there
-				stack2.pack();
-
-				var textures2 = [];
-				for (var m = 0; m < stack2._rawData.length; m++) {
-					var tex = new THREE.DataTexture(
-						stack2.rawData[m],
-						stack2.textureSize,
-						stack2.textureSize,
-						stack2.textureType,
-						THREE.UnsignedByteType,
-						THREE.UVMapping,
-						THREE.ClampToEdgeWrapping,
-						THREE.ClampToEdgeWrapping,
-						THREE.NearestFilter,
-						THREE.NearestFilter);
-					tex.needsUpdate = true;
-					tex.flipY = true;
-					textures2.push(tex);
-				}
-
-				// create material && mesh then add it to sceneLayer1
-				uniformsLayer1 = ShadersDataUniforms.uniforms();
-				uniformsLayer1.uTextureSize.value = stack2.textureSize;
-				uniformsLayer1.uTextureContainer.value = textures2;
-				uniformsLayer1.uWorldToData.value = stack2.lps2IJK;
-				uniformsLayer1.uNumberOfChannels.value = stack2.numberOfChannels;
-				uniformsLayer1.uPixelType.value = stack2.pixelType;
-				uniformsLayer1.uBitsAllocated.value = stack2.bitsAllocated;
-				uniformsLayer1.uWindowCenterWidth.value =
-					[stack2.windowCenter, stack2.windowWidth];
-				uniformsLayer1.uRescaleSlopeIntercept.value =
-					[stack2.rescaleSlope, stack2.rescaleIntercept];
-				uniformsLayer1.uDataDimensions.value = [stack2.dimensionsIJK.x,
-				stack2.dimensionsIJK.y,
-				stack2.dimensionsIJK.z];
-				uniformsLayer1.uInterpolation.value = 0;
-
-				// generate shaders on-demand!
-				var fs = new ShadersDataFragment(uniformsLayer1);
-				var vs = new ShadersDataVertex();
-				materialLayer1 = new THREE.ShaderMaterial(
-					{
-						side: THREE.DoubleSide,
-						uniforms: uniformsLayer1,
-						vertexShader: vs.compute(),
-						fragmentShader: fs.compute(),
-					});
-
-				// add mesh in this scene with right shaders...
-				meshLayer1 = new THREE.Mesh(stackHelper.slice.geometry, materialLayer1);
-				// go the LPS space
-				meshLayer1.applyMatrix(stack._ijk2LPS);
-				sceneLayer1.add(meshLayer1);
-
-				// Create the Mix layer
-				uniformsLayerMix = ShadersLayerUniforms.uniforms();
-				uniformsLayerMix.uTextureBackTest0.value = sceneLayer0TextureTarget.texture;
-				uniformsLayerMix.uTextureBackTest1.value = sceneLayer1TextureTarget.texture;
-
-				let fls = new ShadersLayerFragment(uniformsLayerMix);
-				let vls = new ShadersLayerVertex();
-				materialLayerMix = new THREE.ShaderMaterial(
-					{
-						side: THREE.DoubleSide,
-						uniforms: uniformsLayerMix,
-						vertexShader: vls.compute(),
-						fragmentShader: fls.compute(),
-						transparent: true,
-					});
-
-				// add mesh in this scene with right shaders...
-				meshLayerMix = new THREE.Mesh(stackHelper.slice.geometry, materialLayer1);
-				// go the LPS space
-				meshLayerMix.applyMatrix(stack._ijk2LPS);
-				sceneLayerMix.add(meshLayerMix);
-
-				//
-				// set camera
-				var worldbb = stack.worldBoundingBox();
-				var lpsDims = new THREE.Vector3(
-					worldbb[1] - worldbb[0],
-					worldbb[3] - worldbb[2],
-					worldbb[5] - worldbb[4]
-				);
-
-				// box: {halfDimensions, center}
-				var box = {
-					center: stack.worldCenter().clone(),
-					halfDimensions:
-					new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10),
-				};
-
-				// init and zoom
-				var canvas = {
-					width: threeD.clientWidth,
-					height: threeD.clientHeight,
-				};
-				camera.directions = [stack.xCosine, stack.yCosine, stack.zCosine];
-				camera.box = box;
-				camera.canvas = canvas;
-				camera.update();
-				camera.fitBox(2);
-
-				// CREATE LUT
-				lutLayer0 = new HelpersLut(
-					'my-lut-canvases-l0',
-					'default',
-					'linear',
-					[[0, 0, 0, 0], [1, 1, 1, 1]],
-					[[0, 1], [1, 1]]);
-				lutLayer0.luts = HelpersLut.presetLuts();
-
-				lutLayer1 = new HelpersLut(
-					'my-lut-canvases-l1',
-					'default',
-					'linear',
-					stack2.segmentationLUT,
-					stack2.segmentationLUTO,
-					true);
-				uniformsLayer1.uLut.value = 1;
-				uniformsLayer1.uTextureLUT.value = lutLayer1.texture;
-
-				buildGUI(stackHelper);
-			}
+			var files = t2.map(function(v) {
+				return 'geppetto/extensions/geppetto-hm/samples/lowResPACStypical/' + v;
+			});
 
 			loader.load(files)
 				.then(function () {
-					handleSeries();
+					// merge files into clean series/stack/frame structure
+					var series = loader.data[0].mergeSeries(loader.data);
+					var stack = series[0].stack[0];
+					loader.free();
+					loader = null;
+					// be carefull that series and target stack exist!
+					var stackHelper = new HelpersStack(stack);
+					// stackHelper.orientation = 2;
+					// stackHelper.index = 56;
+
+					// tune bounding box
+					stackHelper.bbox.visible = false;
+
+					// tune slice border
+					stackHelper.border.color = 0xFF9800;
+					// stackHelper.border.visible = false;
+
+					scene.add(stackHelper);
+
+
+					// hook up callbacks
+					controls.addEventListener('OnScroll', function (e) {
+						console.log("scrolling");
+						if (e.delta > 0) {
+							if (stackHelper.index >= stack.dimensionsIJK.z - 1) {
+								return false;
+							}
+							stackHelper.index += 1;
+						} else {
+							if (stackHelper.index <= 0) {
+								return false;
+							}
+							stackHelper.index -= 1;
+						}
+
+					});
+
+					// center camera and interactor to center of bouding box
+					// for nicer experience
+					// set camera
+					var worldbb = stack.worldBoundingBox();
+					var lpsDims = new THREE.Vector3(
+						worldbb[1] - worldbb[0],
+						worldbb[3] - worldbb[2],
+						worldbb[5] - worldbb[4]
+					);
+
+					// box: {halfDimensions, center}
+					var box = {
+						center: stack.worldCenter().clone(),
+						halfDimensions:
+						new THREE.Vector3(lpsDims.x + 10, lpsDims.y + 10, lpsDims.z + 10),
+					};
+
+					// init and zoom
+					var canvas = {
+						width: container.clientWidth,
+						height: container.clientHeight,
+					};
+
+					camera.directions = [stack.xCosine, stack.yCosine, stack.zCosine];
+					camera.box = box;
+					camera.canvas = canvas;
+					camera.update();
+
+					// Not working properly. See issue: https://github.com/FNNDSC/ami/issues/120
+					camera.fitBox(2,2);
 				})
 				.catch(function (error) {
 					window.console.log('oops... something went wrong...');
 					window.console.log(error);
 				});
+
+
+
 
 		},
 
@@ -472,10 +208,6 @@ define(function (require) {
 			return (
 				<div>
 					<div id="dicomViewer">
-					</div>
-					<div id="my-lut-container">
-						<div>CT<div id="my-lut-canvases-l0"></div></div>
-						<div>Segmentation<div id="my-lut-canvases-l1"></div></div>
 					</div>
 				</div>
 			)

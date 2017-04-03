08e35f4706aa8eda941734d3b8608beeb0dd6d90
johnidol <giovanni.idili@gmail.com>
2017-04-03 20:04:36 +0100

    formatting from hell

--- src/main/webapp/js/common/GEPPETTO.Utility.js
+++ src/main/webapp/js/common/GEPPETTO.Utility.js
@@ -208,10 +208,7 @@ define(function (require) {
             String.prototype.format = function() {
                 var args = arguments;
                 return this.replace(/{(\d+)}/g, function(match, number) {
-                    return typeof args[number] != 'undefined'
-                        ? args[number]
-                        : match
-                        ;
+                    return typeof args[number] != 'undefined' ? args[number] : match;
                 });
             };
         }

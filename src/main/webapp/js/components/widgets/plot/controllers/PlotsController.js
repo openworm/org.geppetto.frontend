7f8fd4c7275418b2ae58d57fe2cf9f12cac69ebd
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-04-09 20:17:30 +0100

    Minor changes

--- src/main/webapp/js/components/widgets/plot/controllers/PlotsController.js
+++ src/main/webapp/js/components/widgets/plot/controllers/PlotsController.js
@@ -1,10 +1,9 @@
-
 /**
  * Controller class for plotting widget. Use to make calls to widget from inside Geppetto.
  *
  * @author Jesus R Martinez (jesus@metacell.us)
  */
-define(function (require) {
+define(function(require) {
 
     var AWidgetController = require('../../AWidgetController');
     var Plot = require('../Plot');
@@ -14,7 +13,7 @@ define(function (require) {
      */
     return AWidgetController.View.extend({
 
-        initialize: function () {
+        initialize: function() {
             this.widgets = [];
             var widgets = this.widgets;
         },
@@ -22,18 +21,18 @@ define(function (require) {
         /**
          * Creates plotting widget
          */
-        addPlotWidget: function () {
+        addPlotWidget: function() {
 
             //look for a name and id for the new widget
             var id = this.getAvailableWidgetId("Plot", this.widgets);
             var name = id;
 
             //create plotting widget
-            var p = window[name] = new Plot({id: id, name: name, visible: true});
+            var p = window[name] = new Plot({ id: id, name: name, visible: true });
             p.setController(this);
 
             //create help command for plot
-            p.help = function () {
+            p.help = function() {
                 return GEPPETTO.Console.getObjectCommands(id);
             };
 
@@ -45,7 +44,7 @@ define(function (require) {
             //add commands to console autocomplete and help option
             GEPPETTO.Console.updateHelpCommand(p, id, this.getFileComments("geppetto/js/components/widgets/plot/Plot.js"));
             //update tags for autocompletion
-            GEPPETTO.Console.updateTags(p.getId(), p);            
+            GEPPETTO.Console.updateTags(p.getId(), p);
             return p;
         },
 
@@ -54,7 +53,7 @@ define(function (require) {
          *
          * @param {WIDGET_EVENT_TYPE} event - Event that tells widgets what to do
          */
-        update: function (event, parameters) {
+        update: function(event, parameters) {
             //delete plot widget(s)
             if (event == GEPPETTO.WidgetsListener.WIDGET_EVENT_TYPE.DELETE) {
                 this.removeWidgets();
@@ -79,8 +78,7 @@ define(function (require) {
             }
 
             //update plotting widgets
-            else if (event == GEPPETTO.Events.Experiment_over) {
-            }
+            else if (event == GEPPETTO.Events.Experiment_over) {}
 
             //update plotting widgets
             else if (event == GEPPETTO.Events.Experiment_update) {
@@ -102,7 +100,7 @@ define(function (require) {
          * @param {Node} node - Geppetto Node used for extracting commands
          * @returns {Array} Set of commands associated with this node
          */
-        getCommands: function (node) {
+        getCommands: function(node) {
             var groups = [];
 
             if (node.getWrappedObj().getType().getMetaType() == GEPPETTO.Resources.DYNAMICS_TYPE) {
@@ -149,8 +147,8 @@ define(function (require) {
          * @param path
          * @param plotWidget - options, if not provided a new widget will be created
          */
-        plotStateVariable: function(projectId, experimentId, path, plotWidget){
-            if(window.Project.getId() == projectId && window.Project.getActiveExperiment().getId() == experimentId){
+        plotStateVariable: function(projectId, experimentId, path, plotWidget) {
+            if (window.Project.getId() == projectId && window.Project.getActiveExperiment().getId() == experimentId) {
                 // try to resolve path
                 var inst = undefined;
                 try {
@@ -160,44 +158,44 @@ define(function (require) {
                 // check if we already have data
                 if (inst != undefined && inst.getTimeSeries() != undefined) {
                     // plot, we have data
-                	if(plotWidget != undefined){
-                		plotWidget.plotData(inst);
-                		this.updateLegend(plotWidget,inst,projectId,experimentId);
-                	} else {
-                		var widget = G.addWidget(0);
-                		widget.plotData(inst).setName(path);
-                		this.updateLegend(widget,inst,projectId,experimentId);
+                    if (plotWidget != undefined) {
+                        plotWidget.plotData(inst);
+                        this.updateLegend(plotWidget, inst, projectId, experimentId);
+                    } else {
+                        var widget = G.addWidget(0);
+                        widget.plotData(inst).setName(path);
+                        this.updateLegend(widget, inst, projectId, experimentId);
                     }
                 } else {
-                    var cb = function(){
-                    	var i = window.Instances.getInstance(path);
-                    	if(plotWidget != undefined){
-                    		plotWidget.plotData(i);
-                    		this.updateLegend(plotWidget,i,projectId,experimentId);
-                    	} else {
-                    		G.addWidget(0).plotData(i).setName(path);
-                    	}
+                    var cb = function() {
+                        var i = window.Instances.getInstance(path);
+                        if (plotWidget != undefined) {
+                            plotWidget.plotData(i);
+                            this.updateLegend(plotWidget, i, projectId, experimentId);
+                        } else {
+                            G.addWidget(0).plotData(i).setName(path);
+                        }
                     };
                     // trigger get experiment data with projectId, experimentId and path, and callback to plot
                     GEPPETTO.ExperimentsController.getExperimentState(projectId, experimentId, [path], cb);
                 }
             } else {
-            	var self = this;
+                var self = this;
                 // we are dealing with external instances, define re-usable callback for plotting external instances
-                var plotExternalCallback = function(){
+                var plotExternalCallback = function() {
                     var i = GEPPETTO.ExperimentsController.getExternalInstance(projectId, experimentId, path);
                     var t = GEPPETTO.ExperimentsController.getExternalInstance(projectId, experimentId, 'time(StateVariable)');
-                    
-                    if(plotWidget != undefined){
-                    	plotWidget.plotXYData(i,t);
-                    	self.updateLegend(plotWidget,i,projectId,experimentId);
+
+                    if (plotWidget != undefined) {
+                        plotWidget.plotXYData(i, t);
+                        self.updateLegend(plotWidget, i, projectId, experimentId);
                     } else {
-                    	G.addWidget(0).plotXYData(i,t).setName(path);
+                        G.addWidget(0).plotXYData(i, t).setName(path);
                     }
                 };
 
                 var externalInstance = GEPPETTO.ExperimentsController.getExternalInstance(projectId, experimentId, path);
-                if(externalInstance != undefined){
+                if (externalInstance != undefined) {
                     // if not undefined, plot
                     plotExternalCallback();
                 } else {
@@ -206,62 +204,62 @@ define(function (require) {
                 }
             }
         },
-        
+
         //Updates the legend for a given instance, and updates it in widget
-        updateLegend : function(widget,instance,projectId, experimentId){
-            var legend=null;
+        updateLegend: function(widget, instance, projectId, experimentId) {
+            var legend = null;
             var experimentName, projectName;
-            if(window.Project.getId() == projectId){
-            	if(window.Project.getActiveExperiment()!= null || undefined){
-            		//variable belongs to same project,but different experiment
-            		if(window.Project.getActiveExperiment.getId()!= experimentId){
-            			legend = this.getLegendName(projectId,experimentId,instance,true);
-            		}
-            	}
-            }else{
-            	//variablel belongs to different project and different experiment
-            	var experimentPath = projectName + " - " + experimentName;
-    			legend = this.getLegendName(projectId,experimentId,instance,false);
+            if (window.Project.getId() == projectId) {
+                if (window.Project.getActiveExperiment() != null && window.Project.getActiveExperiment() != undefined) {
+                    //variable belongs to same project,but different experiment
+                    if (window.Project.getActiveExperiment().getId() != experimentId) {
+                        legend = this.getLegendName(projectId, experimentId, instance, true);
+                    }
+                }
+            } else {
+                //variablel belongs to different project and different experiment
+                var experimentPath = projectName + " - " + experimentName;
+                legend = this.getLegendName(projectId, experimentId, instance, false);
             }
-            
+
             //set legend if it needs to be updated, only null if variable belong to active experiment
-            if(legend!=null){
-            	widget.setLegend(instance,legend);
+            if (legend != null) {
+                widget.setLegend(instance, legend);
             }
         },
-        
-        //Returns legend with appropriate project and experiment name in brackets
-        getLegendName : function(projectId, experimentId, instance,sameProject){
-        	var legend=null;
-        	//the variable's experiment belong to same project but it's not the active one
-        	if(sameProject){
-        		for(var key in window.Project.getExperiments()){
-        			if( window.Project.getExperiments()[key].id == experimentId){
-        				//create legend with experiment name
-        				legend = instance.getInstancePath()+" ["+window.Project.getExperiments()[key].name+"]";
-        			}
-        		}
-        	}
-        	//The variable's experiment and projects aren't the one that is active
-        	else{
-        		//get user projects
-        		var projects = GEPPETTO.ProjectsController.getUserProjects();
 
-        		for (var i = 0; i < projects.length; i++) {
-        			//match variable project id
-        			if ((projects[i].id == projectId)) {
-        				//match variable experiment id
-        				for(var key in projects[i].experiments){
-                			if(projects[i].experiments[key].id == experimentId){
-                				//create legend with project name and experiment
-                				legend = instance.getInstancePath()+" ["+projects[i].name + " - " + projects[i].experiments[key].name+"]";
-                			}
-                		}
-        			}
-        		}
-        	}
-        	
-        	return legend;
+        //Returns legend with appropriate project and experiment name in brackets
+        getLegendName: function(projectId, experimentId, instance, sameProject) {
+            var legend = null;
+            //the variable's experiment belong to same project but it's not the active one
+            if (sameProject) {
+                for (var key in window.Project.getExperiments()) {
+                    if (window.Project.getExperiments()[key].id == experimentId) {
+                        //create legend with experiment name
+                        legend = instance.getInstancePath() + " [" + window.Project.getExperiments()[key].name + "]";
+                    }
+                }
+            }
+            //The variable's experiment and projects aren't the one that is active
+            else {
+                //get user projects
+                var projects = GEPPETTO.ProjectsController.getUserProjects();
+
+                for (var i = 0; i < projects.length; i++) {
+                    //match variable project id
+                    if ((projects[i].id == projectId)) {
+                        //match variable experiment id
+                        for (var key in projects[i].experiments) {
+                            if (projects[i].experiments[key].id == experimentId) {
+                                //create legend with project name and experiment
+                                legend = instance.getInstancePath() + " [" + projects[i].name + " - " + projects[i].experiments[key].name + "]";
+                            }
+                        }
+                    }
+                }
+            }
+
+            return legend;
         }
     });
 });

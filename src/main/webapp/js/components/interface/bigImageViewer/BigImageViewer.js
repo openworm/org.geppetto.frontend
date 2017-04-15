561b8ec63cca3d7768d3ca6cfa4efecd86f35af9
Adrian Quintana <adrianquintana@gmail.com>
2017-04-15 14:57:43 +0100

    add toolbar and navigator

--- src/main/webapp/js/components/interface/bigImageViewer/BigImageViewer.js
+++ src/main/webapp/js/components/interface/bigImageViewer/BigImageViewer.js
@@ -20,13 +20,17 @@ define(function (require) {
 				id: this.props.id + "_component",
 				// FIXME: I have copied the images inside the images component folder. More info https://github.com/openseadragon/openseadragon/issues/792
 				prefixUrl: "geppetto/js/components/interface/bigImageViewer/images/",
-				tileSources: this.props.file
+				tileSources: this.props.file,
+				toolbar: "toolbarDiv",
+				showNavigator: this.props.showNavigator
 			})
 		},
 
 		render: function () {
 			return (
 				<div key={this.props.id + "_component"} id={this.props.id + "_component"} className="bigImageViewer">
+					<div id="toolbarDiv" style={{position: 'absolute', top: -1, left: -1, zIndex: 999}}>
+					</div>
 				</div>
 			)
 		}

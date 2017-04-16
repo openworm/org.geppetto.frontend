4f2391dcb25887cbc99be2e4cd906932f64de5db
Adrian Quintana <adrianquintana@gmail.com>
2017-04-16 01:21:43 +0100

    remove css for now

--- src/main/webapp/js/components/interface/carousel/Carousel.js
+++ src/main/webapp/js/components/interface/carousel/Carousel.js
@@ -1,10 +1,10 @@
 define(function (require) {
 
-	var link = document.createElement("link");
-	link.type = "text/css";
-	link.rel = "stylesheet";
-	link.href = "geppetto/js/components/interface/bigImageViewer/Carousel.css";
-	document.getElementsByTagName("head")[0].appendChild(link);
+	// var link = document.createElement("link");
+	// link.type = "text/css";
+	// link.rel = "stylesheet";
+	// link.href = "geppetto/js/components/interface/carousel/Carousel.css";
+	// document.getElementsByTagName("head")[0].appendChild(link);
 
 	var React = require('react');
 

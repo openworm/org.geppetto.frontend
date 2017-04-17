419ff56c04d0baf2bda9ff85fffe7f5ec577e81e
Adrian Quintana <adrianquintana@gmail.com>
2017-04-17 22:29:23 +0100

    remove unneeded script

--- test_npm_build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd src/main/webapp
-npm install
-npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/
-npm run build -- --contextPath=/ --useSsl=true --embedded=false --embedderURL=/
-cd -

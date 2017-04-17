4925626c025aa55d432ffed2fce3c6598973adf8
Adrian Quintana <adrianquintana@gmail.com>
2017-04-17 21:59:44 +0100

    reverting changes to script

--- test_npm_build.sh
+++ test_npm_build.sh
@@ -2,5 +2,6 @@
 
 cd src/main/webapp
 npm install
-npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/ || exit 1
+npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/
+npm run build -- --contextPath=/ --useSsl=true --embedded=false --embedderURL=/
 cd -

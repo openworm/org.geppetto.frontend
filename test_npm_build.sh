4ec29a6ba4326c2c03dcc6e3b46e1501a8edeb1b
Adrian Quintana <adrianquintana@gmail.com>
2017-04-17 20:34:29 +0100

    it is a script not a readme!

--- test_npm_build.sh
+++ test_npm_build.sh
@@ -1,5 +1,7 @@
-- cd src/main/webapp
-- npm install
-- npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/
-- npm run build -- --contextPath=/ --useSsl=true --embedded=false --embedderURL=/ 
-- cd -
+#!/bin/bash
+
+cd src/main/webapp
+npm install
+npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/
+npm run build -- --contextPath=/ --useSsl=true --embedded=false --embedderURL=/ 
+cd -

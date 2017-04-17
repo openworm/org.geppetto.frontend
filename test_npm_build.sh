61fedd66b7aa3a733f6cdb28b1a27485ce0e006c
Adrian Quintana <adrianquintana@gmail.com>
2017-04-17 20:16:08 +0100

    attempt to make travis stops on npm error

---
+++ test_npm_build.sh
@@ -0,0 +1,5 @@
+- cd src/main/webapp
+- npm install
+- npm run build-dev  -- --contextPath=org.geppetto.frontend --useSsl=false --embedded=false --embedderURL=/
+- npm run build -- --contextPath=/ --useSsl=true --embedded=false --embedderURL=/ 
+- cd -

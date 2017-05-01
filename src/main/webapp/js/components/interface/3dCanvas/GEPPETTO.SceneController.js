66600401d7748196b1899801c54cce3076560f78
Rob Court <robbie.court@gmail.com>
2017-05-01 13:04:21 +0100

    removing: move to selection

--- src/main/webapp/js/components/interface/3dCanvas/GEPPETTO.SceneController.js
+++ src/main/webapp/js/components/interface/3dCanvas/GEPPETTO.SceneController.js
@@ -215,24 +215,24 @@ define(function (require) {
                                     if (child.hasOwnProperty("material")) {
                                         GEPPETTO.SceneController.setThreeColor(child.material.color, GEPPETTO.Resources.COLORS.SELECTED);
                                         child.material.opacity = Math.max(0.5, child.material.defaultOpacity);
-                                        child.geometry.computeBoundingBox();
-                                        GEPPETTO.getVARS().controls.target.copy(child.position);
-                                        GEPPETTO.getVARS().controls.target.add(child.geometry.boundingBox.getCenter());
+                                        //child.geometry.computeBoundingBox();
+                                        //GEPPETTO.getVARS().controls.target.copy(child.position);
+                                        //GEPPETTO.getVARS().controls.target.add(child.geometry.boundingBox.getCenter());
                                     }
                                 });
                             } else {
                                 GEPPETTO.SceneController.setThreeColor(mesh.material.color, GEPPETTO.Resources.COLORS.SELECTED);
                                 mesh.material.opacity = Math.max(0.5, mesh.material.defaultOpacity);
-                                mesh.geometry.computeBoundingBox();
-                                //let's set the center of rotation to the selected mesh
-                                GEPPETTO.getVARS().controls.target.copy(mesh.position);
-                                GEPPETTO.getVARS().controls.target.add(mesh.geometry.boundingBox.getCenter());
+                                //mesh.geometry.computeBoundingBox();
+                                ////let's set the center of rotation to the selected mesh
+                                //GEPPETTO.getVARS().controls.target.copy(mesh.position);
+                                //GEPPETTO.getVARS().controls.target.add(mesh.geometry.boundingBox.getCenter());
                             }
                             mesh.selected = true;
                             mesh.ghosted = false;
 
                             
-                            GEPPETTO.getVARS().camera.updateProjectionMatrix();
+                            //GEPPETTO.getVARS().camera.updateProjectionMatrix();
                             
                         }
                         if(GEPPETTO.isKeyPressed('z')){

0529d8c12a193db14bab2d90f9e041cdb2fcf6ec
Adrian Quintana <adrianquintana@gmail.com>
2017-05-04 22:24:19 +0100

    changing regexp for windows

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -141,7 +141,7 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /\.(js)$/, exclude: [/node_modules\/(?!(ami.js)\/).*/, /build/, /\.bundle/], loader: ['babel-loader'],
+                test: /\.(js)$/, exclude: [/node_modules(?!(\/|\\)ami.js)/, /build/, /\.bundle/], loader: ['babel-loader'],
                 query: {
                     presets: ['react', 'es2015']
                 }

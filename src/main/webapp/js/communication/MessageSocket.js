5fd88a28f5eba4fb46a045e4373eb6eae2a94311
Adrian Quintana <adrianquintana@gmail.com>
2017-05-22 00:50:06 +0100

    use file saver library

--- src/main/webapp/js/communication/MessageSocket.js
+++ src/main/webapp/js/communication/MessageSocket.js
@@ -1,41 +1,9 @@
-/*******************************************************************************
- * The MIT License (MIT)
- *
- * Copyright (c) 2011, 2013 OpenWorm.
- * http://openworm.org
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the MIT License
- * which accompanies this distribution, and is available at
- * http://opensource.org/licenses/MIT
- *
- * Contributors:
- *      OpenWorm - http://openworm.org/people.html
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
- * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
- * USE OR OTHER DEALINGS IN THE SOFTWARE.
- ******************************************************************************
+/*
  *
  * WebSocket class use for communication between client and server
  *
  * @author  Jesus R. Martinez (jesus@metacell.us)
  */
-
 
 define(function (require) {
 
@@ -46,6 +14,7 @@ define(function (require) {
         var nextID = 0;
         var connectionInterval = 300;
         var pako = require("pako");
+        var FileSaver = require('file-saver');
         
         var callbackHandler = {};
 
@@ -266,7 +235,7 @@ define(function (require) {
                 var fileNameLength = messageBytes[1];
                 var fileName = String.fromCharCode.apply(null, messageBytes.subarray(2, 2 + fileNameLength));
                 var blob = new Blob([message]);
-                GEPPETTO.Utility.saveData(blob.slice(2 + fileNameLength), fileName);
+                FileSaver.saveAs(blob.slice(2 + fileNameLength), fileName);
             }
         }
     }

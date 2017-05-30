3b856484a3a828c960618c53fa6575c3ea53ffe5
Adrian Quintana <adrianquintana@gmail.com>
2017-05-30 17:03:33 +0100

    adding editorconfig and customising eslint

--- src/main/webapp/.eslintrc.js
+++ src/main/webapp/.eslintrc.js
@@ -10,12 +10,19 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // Comma at the end of a dictionary
     'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'unix'],
+    // Indent must be four space
+    indent: ['error', 4],
+    // Not sure what this does...
+    //'linebreak-style': ['error', 'unix'],
+    // Single quotes
     quotes: ['error', 'single'],
+    // Semi colon end of line
     semi: ['error', 'always'],
+    // Unused variable spits out a warning
     'no-unused-vars': ['warn'],
+    // Allow to use console
     'no-console': 0,
   },
 };

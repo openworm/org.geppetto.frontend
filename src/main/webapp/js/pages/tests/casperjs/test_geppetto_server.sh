13ae1f5ef8e93cdc3cd5bf2b3fec7434dd511bea
jrmartin <yeshuarmartinez@gmail.com>
2017-07-08 22:27:06 -0700

    test output

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -3,7 +3,8 @@ while ! curl http://0.0.0.0:8080/org.geppetto.frontend
 do
   echo "Waiting for docker to finish building.";
   docker ps;
+  curl -Is "http://0.0.0.0:8080/org.geppetto.frontend"
+  curl -Is "http://localhost:8080/org.geppetto.frontend"
   sleep 1
 done
 echo "$(date) - connected successfully"
-curl -L http://0.0.0.0:8080/org.geppetto.frontend

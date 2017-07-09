f24ebc6418d131e9b65d4fbd33279344b4c8006a
jrmartin <yeshuarmartinez@gmail.com>
2017-07-08 21:20:22 -0700

    test

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 while ! curl http://0.0.0.0:8080/org.geppetto.frontend
 do
-  echo "$(date) - still trying";
+  echo "Waiting for docker to finish building.";
   docker ps;
-  docker-machine ip;
   sleep 1
 done
 echo "$(date) - connected successfully"
+curl -L http://0.0.0.0:8080/org.geppetto.frontend

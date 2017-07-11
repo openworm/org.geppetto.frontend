253843114dfe94f0b24a83aeb16934898ed4edf9
jrmartin <yeshuarmartinez@gmail.com>
2017-07-11 10:09:51 -0700

    update host for casper persstence

--- src/main/webapp/js/pages/tests/casperjs/UITests.js
+++ src/main/webapp/js/pages/tests/casperjs/UITests.js
@@ -1,5 +1,7 @@
-var TARGET_URL = "http://127.0.0.1";
-var port = ":8080";
+var urlBase = casper.cli.get('host');
+if(urlBase==null || urlBase==undefined){
+	urlBase = "http://127.0.0.1:8080/";
+}
 var PROJECT_URL_SUFFIX = "?load_project_from_url=https://raw.githubusercontent.com/openworm/org.geppetto.samples/development/UsedInUnitTests/SingleComponentHH/GEPPETTO.json";
 var PROJECT_URL_SUFFIX_2 = "?load_project_from_url=https://raw.githubusercontent.com/openworm/org.geppetto.samples/development/UsedInUnitTests/pharyngeal/project.json";
 var PROJECT_URL_SUFFIX_3 = "?load_project_from_url=https://raw.githubusercontent.com/openworm/org.geppetto.samples/development/UsedInUnitTests/balanced/project.json";
@@ -28,7 +30,7 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
         }
     });
 
-    casper.start(TARGET_URL + port+"/org.geppetto.frontend", function () {
+    casper.start(urlBase+"org.geppetto.frontend", function () {
         this.waitForSelector('div#logo', function () {
             this.echo("I waited for the logo to load.");
             test.assertTitle("geppetto's home", "geppetto's homepage title is the one expected");
@@ -36,14 +38,14 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
         }, null, 30000);
     });
 
-    casper.thenOpen(TARGET_URL + port+"/org.geppetto.frontend/login?username=guest1&password=guest", function () {
+    casper.thenOpen(urlBase+"org.geppetto.frontend/login?username=guest1&password=guest", function () {
         /*this.waitForSelector('div#page', function() {
          this.echo("I've waited for the splash screen to come up.");
          test.assertUrlMatch(/splash$/, 'Virgo Splash Screen comes up indicating successful login');
          }, null, 30000);*/
     });
 
-    casper.thenOpen(TARGET_URL+  port+"/org.geppetto.frontend/", function () {
+    casper.thenOpen(urlBase+"org.geppetto.frontend/", function () {
         this.waitForSelector('div[project-id="2"]', function () {
             this.echo("I've waited for the projects to load.");
             test.assertExists('div#logo', "logo is found");
@@ -53,7 +55,7 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
     });
 
     casper.then(function () {
-        testProject(test, TARGET_URL + port+"/org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX, true,
+        testProject(test, urlBase+"org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX, true,
             false, 'hhcell.hhpop[0].v', 'Model.neuroml.pulseGen1.delay', true,"hhcell");
     });
     
@@ -65,15 +67,15 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
     });
     
     casper.then(function () {
-        reloadProjectTest(test, TARGET_URL + port+"/org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
+        reloadProjectTest(test, urlBase+"org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
     });
     
     casper.then(function () {
-        deleteProject(test, TARGET_URL + port+"/org.geppetto.frontend",projectID);
+        deleteProject(test, urlBase+"org.geppetto.frontend",projectID);
     });
 
     casper.then(function () {
-        testProject(test, TARGET_URL + port+"/org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX_2, false,
+        testProject(test, urlBase+"org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX_2, false,
             false, 'c302_A_Pharyngeal.M1[0].v', 'Model.neuroml.generic_neuron_iaf_cell.C', false,"c302_A_Pharyngeal");
     });
     
@@ -85,15 +87,15 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
     });
     
     casper.then(function () {
-        reloadProjectTest(test, TARGET_URL + port+"/org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
+        reloadProjectTest(test, urlBase+"org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
     });
     
     casper.then(function () {
-        deleteProject(test, TARGET_URL + port+"/org.geppetto.frontend",projectID);
+        deleteProject(test, urlBase+"org.geppetto.frontend",projectID);
     });
 
     casper.then(function () {
-        testProject(test, TARGET_URL  +port+"/org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX_3, false,
+        testProject(test, urlBase+"org.geppetto.frontend/geppetto" + PROJECT_URL_SUFFIX_3, false,
             false, '', '', false,"Balanced_240cells_36926conns");
     });
     
@@ -105,23 +107,23 @@ casper.test.begin('Geppetto basic tests', 133, function suite(test) {
     });
     
     casper.then(function () {
-        reloadProjectTest(test, TARGET_URL + port+"/org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
+        reloadProjectTest(test, urlBase+"org.geppetto.frontend/geppetto?load_project_from_id="+projectID,1);
     });
-    
+   
     casper.then(function () {
-        deleteProject(test, TARGET_URL + port+"/org.geppetto.frontend",projectID);
+        deleteProject(test, urlBase+"org.geppetto.frontend",projectID);
     });
 
     //TODO: log back in as other users. Check more things
     //TODO: exercise the run loop, check the changing experiment status, try to make experiment fail
 
-    casper.thenOpen(TARGET_URL + port+"/org.geppetto.frontend/logout", function () {
+    casper.thenOpen(urlBase+"org.geppetto.frontend/logout", function () {
     });
     
-    casper.thenOpen(TARGET_URL + port+"/org.geppetto.frontend/login?username=admin&password=admin", function () {
+    casper.thenOpen(urlBase+"org.geppetto.frontend/login?username=admin&password=admin", function () {
     });
 
-    casper.thenOpen(TARGET_URL+  port+"/org.geppetto.frontend/admin", function () {
+    casper.thenOpen(urlBase+"org.geppetto.frontend/admin", function () {
         this.waitForSelector('div[class="griddle"]', function () {
             this.echo("I've waited for the admin panel to load.");
         }, null, 30000);

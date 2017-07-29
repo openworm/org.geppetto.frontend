6c99d4c1910506ceb4c8826d20966d74af090cea
jrmartin <yeshuarmartinez@gmail.com>
2017-07-29 11:22:18 -0700

    test

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-while ! curl http://localhost:28081/org.geppetto.frontend
-do
-  echo "Waiting for docker to finish building.";
+while curl -s -o /dev/null -w "%{http_code}" http://localhost:28081/org.geppetto.frontend == 404 or curl -s -o /dev/null -w "%{http_code}" http://localhost:28081/org.geppetto.frontend == 000
+do 
+  echo "Waiting for url host to be up.";
   sleep 60
 done
 echo "$(date) - connected successfully" && curl -Is "http://localhost:28081/org.geppetto.frontend"

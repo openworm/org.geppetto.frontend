b8ec5a6eca292e506434edf7f4e2e0112ff3809f
jrmartin <yeshuarmartinez@gmail.com>
2017-07-29 16:32:02 -0700

    test new script

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-while ! curl http://localhost:28081/org.geppetto.frontend
-do
-  echo "Waiting for docker to finish building.";
-  sleep 60
-done
-echo "$(date) - connected successfully" && curl -Is "http://localhost:28081/org.geppetto.frontend"
+Test () {
+	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://192.168.99.100:28081/org.geppetto.frontend')"
+		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}')
+		echo $http_status
+		if [ "$http_status" == "302" ]; then
+			echo "$(date) - connected successfully!"
+		else
+			echo "Waiting for docker to finish building.";
+  			sleep 15
+  			Test
+		fi
+}
+
+Test

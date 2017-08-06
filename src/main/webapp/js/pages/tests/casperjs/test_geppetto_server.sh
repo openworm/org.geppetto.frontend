36d76fcd065273029e2e2ba5e02a4a149afd0944
jrmartin <yeshuarmartinez@gmail.com>
2017-08-06 16:56:57 -0700

    not liking semicolons everywhere

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 iterations=1
 Test () {
-	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:28081/org.geppetto.frontend')";
-		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}');
+	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:28081/org.geppetto.frontend')"
+		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}')
 		echo $http_status;
 		if [ "$http_status" == "302" ]; then
 			echo "$(date) - connected successfully!";
 		else
 			if [ "$iterations" == "10" ]; then
-				exit 0;
+				exit 4;
 			else
 				echo "Waiting for docker to finish building.";
 				iterations=$((iterations+1));

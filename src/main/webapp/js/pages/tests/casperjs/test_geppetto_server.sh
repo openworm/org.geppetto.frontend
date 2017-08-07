ee96a65f55f8cbdec3ef091b6200fab6bb655301
jrmartin <yeshuarmartinez@gmail.com>
2017-08-07 16:51:23 -0700

    test attempt to fix script

--- src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/test_geppetto_server.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 iterations=1
 Test () {
-	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:28081/org.geppetto.frontend')"
-		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}')
+	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code=='http://localhost:28081/org.geppetto.frontend')";
+		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}');
 		echo "$http_status";
 		echo "Waiting for docker to finish building.";
 		if [ "$http_status" == "302" ]; then
 			echo "$(date) - connected successfully!"
 		else
-			if [ "$iterations" == "100" ]; then
+			if [ "$iterations" == "10" ]; then
 				exit 0
 			else
 				iterations=$((iterations+1))
-  				sleep 1
+  				sleep 15
   				Test
 			fi
 		fi

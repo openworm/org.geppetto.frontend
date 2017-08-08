0cd3b7577e6047766cd04310a5a387a3b51a5e73
Rob Court <rcourt@ed.ac.uk>
2017-08-08 11:42:47 +0100

    Adding new features to help

--- src/main/webapp/js/components/widgets/stackViewer/StackViewer.js
+++ src/main/webapp/js/components/widgets/stackViewer/StackViewer.js
@@ -256,8 +256,10 @@ define(function (require) {
 	    'Hovering shows items that can be added by clicking in combination with the shift key. \n\n' +
 	    'Clicking on any item displayed in the stack selects it. \n\n' +
             'Zoom level is controlled by either 2 finger pinch, ctrl + mouse wheel or the <i class="fa fa-search-plus" aria-hidden="true"></i> / <i class="fa fa-search-minus" aria-hidden="true"></i> icons. \n\n' +
-	    'Stack position is controlled via 2 finger scroll, mouse wheel or the <i class="fa fa-chevron-up" aria-hidden="true"></i> / <i class="fa fa-chevron-down" aria-hidden="true"></i> icons. \n\n' + 
-            'The orthogonal orientation of the slice is toggled by the <i class="fa fa-refresh" aria-hidden="true"></i> icon.';
+	    'Stack position is controlled via 2 finger scroll, mouse wheel or the <i class="fa fa-chevron-up" aria-hidden="true"></i> / <i class="fa fa-chevron-down" aria-hidden="true"></i> icons. \n' + 
+	    '<b>Note:</b> Both button and mouse wheel navigation through the stack can be accelerated by pressing the shift key in combination. \n' + 
+            'The orthogonal orientation of the slice is toggled by the <i class="fa fa-refresh" aria-hidden="true"></i> icon. \n' + 
+            'The botton toggle switch controlls the display of the current slice within the 3D window giving your current selection context within the volume';
         },
 
         destroy: function () {

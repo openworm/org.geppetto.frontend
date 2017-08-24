767687091d8ca6406fb49f31ce5b8b3c5a538dfa
Adrian Quintana <adrianquintana@gmail.com>
2017-08-24 01:54:30 +0100

    remove all icons except download and close, go straight to full size, reduce opacity for background, unmount gallery on close, create gallery in a declarative way

--- src/main/webapp/js/components/interface/gallery/Gallery.js
+++ src/main/webapp/js/components/interface/gallery/Gallery.js
@@ -7,7 +7,6 @@ define(function (require) {
 	require('lightgallery');
 	require('lg-thumbnail');
 	require('lg-video');
-	require('lg-zoom');
 	require('lg-fullscreen');
 	require('lg-pager');
 	require('../../../../node_modules/lightgallery/src/css/lightgallery.css');
@@ -23,39 +22,75 @@ define(function (require) {
 		}
 
 		componentDidMount() {
-			if ($('#lightgallery').data('lightGallery')) $('#lightgallery').data('lightGallery').destroy(true)
-			$('#lightgallery').lightGallery({
-				thumbnail: true
+
+			var dynamicEl = [];
+			this.state.items.forEach(function (item) {
+				switch (item.type) {
+					case 'image':
+						dynamicEl.push({
+							"src": item.original,
+							'thumb': item.thumbnail,
+							'subHtml': item.caption
+						});
+						break;
+
+					case 'video-youtube':
+						dynamicEl.push({
+							"src": 'https://www.youtube.com/watch?v=' + item.id,
+							'thumb': (item.thumbnail != undefined) ? item.thumbnail : 'https://img.youtube.com/vi/' + item.id + '/0.jpg',
+							'subHtml': item.caption
+						});
+						break;
+				}
+			});
+
+
+			$('#lightgallery').on('onCloseAfter.lg', function (event) {
+				$('#lightgallery').data('lightGallery').destroy(true);
+			});
+
+			$('#lightgallery').on('onBeforeOpen.lg', function (event, index, fromTouch, fromThumb) {
+				$.fn.lightGallery.modules.fullscreen.prototype.requestFullscreen()
+			});
+
+			$(".gallery-item").on('click', function () {
+				var slideID = $(this).attr('data-slide');
+
+				$('#lightgallery').lightGallery({
+					thumbnail: true,
+					index: parseInt(slideID),
+					dynamic: true,
+					dynamicEl: dynamicEl,
+					fullScreen: false
+				});
+
+
+
 			});
 		}
 
-		componentDidUpdate() {
+		componentWillUnmount() {
 			if ($('#lightgallery').data('lightGallery')) $('#lightgallery').data('lightGallery').destroy(true)
-			$('#lightgallery').lightGallery({
-				thumbnail: true
-			});
 		}
 
 		render() {
 			return (
 				<div key="lightgallery" id="lightgallery" style={this.props.style}>
-					{this.state.items.map((item) => {
+					{this.state.items.map((item, index) => {
 
-						{
-							switch (item.type) {
-								case 'image':
-									return <a href={item.original}>
-										<img src={item.thumbnail} />
-									</a>
-								case 'video-youtube':
-
-									return <a href={'https://www.youtube.com/watch?v=' + item.id}>
-										<img src={
-											(item.thumbnail != undefined) ?
-												item.thumbnail :
-												'https://img.youtube.com/vi/' + item.id + '/0.jpg'} />
-									</a>
-							}
+						switch (item.type) {
+							case 'image':
+								return <a className="gallery-item" data-slide={index}>
+									<img src={item.thumbnail} />
+								</a>
+							case 'video-youtube':
+								return <a className="gallery-item" data-slide={index}>
+									<img data-slide={index} src={
+										(item.thumbnail != undefined) ?
+											item.thumbnail :
+											'https://img.youtube.com/vi/' + item.id + '/0.jpg'}
+									/>
+								</a>
 						}
 					})
 					}

a6b890093b5ad677512f783fc25f0a16c5cdeb71
Adrian Quintana <adrianquintana@gmail.com>
2017-09-05 07:30:48 +0100

    add onclick event

--- src/main/webapp/js/components/interface/htmlViewer/HTMLViewer.js
+++ src/main/webapp/js/components/interface/htmlViewer/HTMLViewer.js
@@ -14,6 +14,8 @@ define(function (require) {
 			this.state = {
 				content: this.props.content,
 			};
+
+			this.handleClick = this.handleClick.bind(this);
 		}
 
 		setContent(content) {
@@ -24,12 +26,19 @@ define(function (require) {
             if (nextProps.content != this.props.content) {
                 this.setState({ content: nextProps.content });
             }
-        }
+		}
+
+		handleClick(e){
+			var $el = $(e.target);
+			if ($el.is('a') && $el.data('action')) {
+				this.props.handleClick($el, $el.data('action'));
+			}
+		}
 
 		render() {
 			return (
 				<div key={this.props.id + "_component"} id={this.props.id + "_component"} className="htmlViewer"  style={this.props.style} className={this.props.class}>
-					<div dangerouslySetInnerHTML={{__html: this.state.content}}></div>
+					<div dangerouslySetInnerHTML={{__html: this.state.content}} onClick={this.handleClick}></div>
 				</div>
 			)
 		}

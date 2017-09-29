a72411bbab6809f1910fd13f332c69c025f57415
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-09-29 13:46:04 +0100

    Comment out bundle analyzer

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+//var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 var geppettoConfig;
 try {

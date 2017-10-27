622fea47189958435f636c4ff250b1da74e23135
jrmartin <yeshuarmartinez@gmail.com>
2017-10-27 10:44:15 -0700

    put back require

--- src/main/webapp/js/pages/tests/casperjs/PersistenceTests.js
+++ src/main/webapp/js/pages/tests/casperjs/PersistenceTests.js
@@ -342,7 +342,7 @@ function testProject(test, url, expect_error, persisted, spotlight_record_variab
 
                     //Good pattern for checking the absence of an attribute
                     test.assertEvalEquals(function () {
-                        return this.getElementAttribute('button.SaveButton', 'disabled');
+                        return require('../../utils').dump(this.getElementAttribute('button.SaveButton', 'disabled'));
                     }, null, "The persist button is correctly active.");
 
                     //Click persist button. Check things again

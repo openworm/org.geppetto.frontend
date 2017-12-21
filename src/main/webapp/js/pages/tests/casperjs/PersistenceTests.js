8fbd46de7e7e4750baebc20503e85685c66b3946
jrmartin <yeshuarmartinez@gmail.com>
2017-12-21 11:32:21 -0800

    test

--- src/main/webapp/js/pages/tests/casperjs/PersistenceTests.js
+++ src/main/webapp/js/pages/tests/casperjs/PersistenceTests.js
@@ -54,7 +54,9 @@ casper.test.begin('Geppetto basic tests', function suite(test) {
     
     casper.then(function () {
         casper.thenOpen(urlBase+"org.geppetto.frontend/login?username=guest1&password=guest", function () {
-        	this.echo("Starting geppetto at host login "+ urlBase);
+        	casper.wait(15000, function () {
+            	this.echo("Starting geppetto at host login "+ urlBase);
+        	});
             /*this.waitForSelector('div#page', function() {
              this.echo("I've waited for the splash screen to come up.");
              test.assertUrlMatch(/splash$/, 'Virgo Splash Screen comes up indicating successful login');

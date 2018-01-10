540823f9b9df236a955de18e2ff10b90ae464ca1
jrmartin <yeshuarmartinez@gmail.com>
2018-01-10 09:43:27 -0800

    contextpath fix for webpack

--- src/main/webapp/js/pages/geppetto/GEPPETTO.Main.js
+++ src/main/webapp/js/pages/geppetto/GEPPETTO.Main.js
@@ -107,7 +107,12 @@ define(function (require) {
              * Initialize web socket communication
              */
             init: function () {
-                GEPPETTO.MessageSocket.connect(GEPPETTO.MessageSocket.protocol + window.location.host + '/' + GEPPETTO_CONFIGURATION.contextPath + '/GeppettoServlet');
+            	var host = GEPPETTO.MessageSocket.protocol + window.location.host + '/' + GEPPETTO_CONFIGURATION.contextPath + '/GeppettoServlet';
+            	if(GEPPETTO_CONFIGURATION.contextPath=="/"){
+            		host = GEPPETTO.MessageSocket.protocol + window.location.host.replace("8081","8080") + '/GeppettoServlet';
+            	}
+                GEPPETTO.MessageSocket.connect(host);
+                console.log("Host for MessageSocket to connect: "+host);
                 GEPPETTO.Events.listen();
                 this.createChannel();
                 GEPPETTO.CommandController.log(GEPPETTO.Resources.GEPPETTO_INITIALIZED, true);

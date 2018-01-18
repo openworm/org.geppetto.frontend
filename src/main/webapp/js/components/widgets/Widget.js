7b974b46e719c21bfd9f73a2f35c530333d458a0
Matt Earnshaw <matt@earnshaw.org.uk>
2018-01-18 17:30:51 +0000

    prototype widget pinning

--- src/main/webapp/js/components/widgets/Widget.js
+++ src/main/webapp/js/components/widgets/Widget.js
@@ -33,6 +33,7 @@ define(function (require) {
             dialog: null,
             visible: true,
             destroyed: false,
+            pinned: false,
             size: {},
             position: {},
             registeredEvents: null,

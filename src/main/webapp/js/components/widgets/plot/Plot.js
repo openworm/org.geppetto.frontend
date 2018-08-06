4885c57a3eb28b82610b264638e35fe35f3e3e84
Matt Earnshaw <matt@earnshaw.org.uk>
2018-08-06 17:42:43 +0100

    strings in view

--- src/main/webapp/js/components/widgets/plot/Plot.js
+++ src/main/webapp/js/components/widgets/plot/Plot.js
@@ -379,7 +379,7 @@ define(function (require) {
 					name: legendName,
 					line: $.extend({
 					    dash: 'solid',
-					    width: 2
+					    width: '2'
 					}, lineOptions),
 					hoverinfo : 'all',
 					type : 'scatter'
@@ -1113,7 +1113,7 @@ define(function (require) {
 					name: options.legendText,
 					line: {
 						dash: 'solid',
-						width: 2
+						width: '2'
 					}
 			};
 
@@ -1173,7 +1173,7 @@ define(function (require) {
 					name: legendName,
 			    line: $.extend({
                                 dash: 'solid',
-				width: 2
+				width: '2',
 			    }, lineOptions),
 					hoverinfo : 'alle'
 			};

5786894758f33c5af7489a5fe112da3b3b618e57
jrmartin <yeshuarmartinez@gmail.com>
2018-08-27 13:27:40 +0200

    use 8080

--- src/main/webapp/js/pages/tests/casperjs/utilities/test_geppetto_server.sh
+++ src/main/webapp/js/pages/tests/casperjs/utilities/test_geppetto_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 iterations=1
 Test () {
-	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:28081/org.geppetto.frontend')"
+	    request_cmd="$(curl -i -o - --silent -X GET --header 'Accept: application/json' --header 'Authorization: _your_auth_code==' 'http://localhost:8080/org.geppetto.frontend')"
 		http_status=$(echo "$request_cmd" | grep HTTP |  awk '{print $2}')
 		echo $http_status
 		if [ "$http_status" == "302" ]; then

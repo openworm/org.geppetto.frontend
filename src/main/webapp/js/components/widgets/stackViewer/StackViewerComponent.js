a40836a72eeb200530bd8b2bab454cf7ce2daa22
Rob Court <robbie.court@gmail.com>
2018-10-17 08:39:55 +0100

    HotFix: null passing fix for labels
    prevents returning 'null' in floatover

--- src/main/webapp/js/components/widgets/stackViewer/StackViewerComponent.js
+++ src/main/webapp/js/components/widgets/stackViewer/StackViewerComponent.js
@@ -370,7 +370,7 @@ define(function (require) {
                                                     }
                                                     break;
                                                 } else {
-                                                    if (typeof that.props.templateDomainIds !== 'undefined' && typeof that.props.templateDomainNames !== 'undefined' && typeof that.props.templateDomainIds[index] !== 'undefined' && typeof that.props.templateDomainNames[index] !== 'undefined') {
+                                                    if (typeof that.props.templateDomainIds !== 'undefined' && typeof that.props.templateDomainNames !== 'undefined' && typeof that.props.templateDomainIds[index] !== 'undefined' && typeof that.props.templateDomainNames[index] !== 'undefined' && that.props.templateDomainIds[index] !== null && that.props.templateDomainNames[index] !== null) {
                                                         try {
                                                             eval(that.state.id[i][Number(result[j])]).select();
                                                             console.log(that.props.templateDomainNames[index] + ' clicked');

65a78df28ca8f47781715e7d601efdf1f123a88c
Adrian Quintana <adrianquintana@gmail.com>
2018-10-22 22:10:06 +0100

    remove comments

--- src/main/webapp/js/communication/geppettoJupyter/GeppettoJupyterUtils.js
+++ src/main/webapp/js/communication/geppettoJupyter/GeppettoJupyterUtils.js
@@ -1,7 +1,3 @@
-
-
-
-
 const handle_output = function (data) {
     //data is the object passed to the callback from the kernel execution
     switch (data.msg_type) {
@@ -13,19 +9,6 @@ const handle_output = function (data) {
             break;
         case 'execute_result':
             GEPPETTO.CommandController.log(data.content.data['text/plain'].trim(), true);
-            //FIXME Couldnt find a method to avoid this string manipulation.
-            //content = content.replace("b'","'");
-            //if(content.startsWith("'") && content.endsWith("'")){
-            //  content=content.substr(1, content.length-1)
-            //}
-            //content = content.replace(/\\n/g, ' ').replace(/\s{2,}/g,' ').replace(/^\s+|\s+$/,'');
-            //GEPPETTO.trigger(GEPPETTO.Events.Receive_Python_Message, { id: data.parent_header.msg_id, type: data.msg_type, response: content });
-
-            // let content = data.content.data['text/plain'];
-            // execPythonCommand('jsonapi.dumps(json_clean(' + content + ')).decode("utf-8")', handle_output2).then((response) => {
-            //     console.log(response)
-            //     GEPPETTO.trigger(GEPPETTO.Events.Receive_Python_Message, { id: data.parent_header.msg_id, type: data.msg_type, response: response });
-            // });
             try {
                 var response = JSON.parse(data.content.data['text/plain'].replace(/^'(.*)'$/, '$1'));
             } catch (error) {
@@ -35,7 +18,6 @@ const handle_output = function (data) {
             break;
         case "display_data":
             //FIXME
-            //     GEPPETTO.trigger(GEPPETTO.Events.Receive_Python_Message, { id: data.parent_header.msg_id, type: data.msg_type, response: data.content.data['image/png'] });
             break;
         default:
             GEPPETTO.CommandController.log(data.content.text.trim(), true);

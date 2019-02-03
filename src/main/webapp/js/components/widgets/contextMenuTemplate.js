d776143044b2969662a58b09dcf560e32c4d74e9
rodriguez-facundo <bruseghini.f@gmail.com>
2019-02-03 11:48:13 -0300

    make geppetto-application a submodule

--- src/main/webapp/js/components/widgets/contextMenuTemplate.js
+++
@@ -1,6 +0,0 @@
-define(function (require) {
-	return {
-		tplContextMenu: '',
-		tplContextMenuItems: '<li id="<%= this.cid %>"><% if (icon != null && icon != undefined) { %><i class="<%= icon %>" id="<%= this.cid %>"></i><% } %> <%= label %></li>',
-	}
-});

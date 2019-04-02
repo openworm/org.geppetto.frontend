8c2f88bb0200bc0727a618b62063ac90d6451705
Dario Del Piano <dario@metacell.us>
2019-04-02 11:51:06 +0100

    wireframe moved inside the canvas

--- src/main/webapp/js/components/interface/cameraControls/CameraControls.js
+++ src/main/webapp/js/components/interface/cameraControls/CameraControls.js
@@ -1,4 +1,4 @@
-define(function(require) {
+define(function (require) {
     var React = require('react');
     var CreateClass = require('create-react-class');
     var GEPPETTO = require('geppetto');
@@ -7,69 +7,73 @@ define(function(require) {
 
     var CameraControls = CreateClass({
 
-        panLeft: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraPan(-0.01, 0)', true);
+        panLeft: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraPan(-0.01, 0)', true);
         },
 
-        panRight: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraPan(0.01, 0)', true);
+        panRight: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraPan(0.01, 0)', true);
         },
 
-        panUp: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraPan(0, -0.01)', true);
+        panUp: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraPan(0, -0.01)', true);
         },
 
-        panDown: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraPan(0, 0.01)', true);
+        panDown: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraPan(0, 0.01)', true);
         },
 
-        rotateUp: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(0, 0.01)', true);
+        rotateUp: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(0, 0.01)', true);
         },
 
-        rotateDown: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(0, -0.01)', true);
+        rotateDown: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(0, -0.01)', true);
         },
 
-        rotateLeft: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(-0.01, 0)', true);
+        rotateLeft: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(-0.01, 0)', true);
         },
 
-        rotateRight: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(0.01, 0)', true);
-        },
-        
-        rotateZ: function() {
-        	GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(0, 0, 0.01)',true);
+        rotateRight: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(0.01, 0)', true);
         },
 
-        rotateMZ: function(increment) {
-        	GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraRotate(0, 0, -0.01)',true);
+        rotateZ: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(0, 0, 0.01)', true);
         },
 
-        rotate: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.autoRotate()', true);
-        },
-        
-        cameraHome: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.resetCamera()', true);
+        rotateMZ: function (increment) {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraRotate(0, 0, -0.01)', true);
         },
 
-        zoomIn: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraZoom(-0.1)',true);
+        rotate: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.autoRotate()', true);
         },
 
-        zoomOut: function() {
-            GEPPETTO.CommandController.execute(this.props.viewer+'.incrementCameraZoom(+0.1)',true);
+        cameraHome: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.resetCamera()', true);
         },
 
-        componentDidMount: function() {
+        zoomIn: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraZoom(-0.1)', true);
+        },
+
+        zoomOut: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.incrementCameraZoom(+0.1)', true);
+        },
+
+        wireFrame: function () {
+            GEPPETTO.CommandController.execute(this.props.viewer + '.setWireframe(!' + this.props.viewer + '.getWireframe())', true);
+        },
+
+        componentDidMount: function () {
 
         },
 
         render: function () {
             return (
-            	<div className="position-toolbar">
+                <div className="position-toolbar">
                     <button id="panLeftBtn" className="btn squareB fa fa-chevron-left pan-left" onClick={this.panLeft}></button>
                     <button id="panUpBtn" className="btn squareB fa fa-chevron-up pan-top" onClick={this.panUp}></button>
                     <button id="panRightBtn" className="btn squareB fa fa-chevron-right pan-right" onClick={this.panRight}></button>
@@ -86,6 +90,7 @@ define(function(require) {
 
                     <button id="zoomInBtn" className="btn squareB fa fa-search-plus zoom-in" onClick={this.zoomIn}></button>
                     <button id="zoomOutBtn" className="btn squareB fa fa-search-minus zoom-out" onClick={this.zoomOut}></button>
+                    <button id="wireframeBtn" className="btn squareB gpt-sphere_wireframe-jpg wireframe" onClick={this.wireFrame}></button>
                 </div>
 
             );
